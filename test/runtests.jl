3ec69bf20fed6c3414d52fe333c6a5bd2fe48ee4
Benoît Legat <benoit.legat@gmail.com>
2016-08-21 15:32:27 +0200

    Add test: sosdemo4

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,5 +16,7 @@ include("solvers.jl")
 include("sosdemo1.jl")
 include("motzkin.jl")
 include("sosdemo2.jl")
+#include("sosdemo3.jl")
+include("sosdemo4.jl")
 
 FactCheck.exitstatus()
---
+++ test/sosdemo4.jl
@@ -0,0 +1,33 @@
+# Adapted from:
+# SOSDEMO4 --- Matrix Copositivity
+# Section 3.4 of SOSTOOLS User's Manual
+
+facts("SOSDEMO1") do
+for solver in sdp_solvers
+context("With solver $(typeof(solver))") do
+  @polyvar x1 x2 x3 x4 x5
+  x = [x1, x2, x3, x4, x5]
+
+  # The matrix under consideration
+  J = [1 -1  1  1 -1;
+      -1  1 -1  1  1;
+       1 -1  1 -1  1;
+       1  1 -1  1 -1;
+      -1  1  1 -1  1]
+
+  xs = x.^2
+  xsJxs = dot(xs, J*xs)
+  r = x1^2 + x2^2 + x3^2 + x4^2 + x5^2;
+
+  m0 = JuMP.Model(solver = solver)
+  @SOSconstraint m0 xsJxs >= 0
+  status = solve(m0)
+  @fact status --> :Infeasible
+
+  m1 = JuMP.Model(solver = solver)
+  @SOSconstraint m1 r*xsJxs >= 0
+  status = solve(m1)
+  @fact status --> :Optimal
+  # Program is feasible. The matrix J is copositive
+
+end; end; end
