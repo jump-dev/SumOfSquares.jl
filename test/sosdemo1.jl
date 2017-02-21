23b6eec98069e712e2335e6dc01347433ac313b6
Benoît Legat <benoit.legat@gmail.com>
2017-02-21 16:38:21 +0100

    Add tests for sosdemo1

--- test/sosdemo1.jl
+++ test/sosdemo1.jl
@@ -9,6 +9,11 @@ context("With solver $(typeof(solver))") do
 
   m = Model(solver = solver)
 
+  # Example 2.4 of
+  # P. Parrilo and A. Jadbabaie
+  # "Approximation of the joint spectral radius using sum of squares."
+  # Linear Algebra and its Applications, Elsevier, 2008, 428, 2385-2402
+
   p = 2*x^4 + 2*x^3*y - x^2*y^2 + 5*y^4
 
   soscon = @polyconstraint m p >= 0
@@ -17,8 +22,15 @@ context("With solver $(typeof(solver))") do
 
   @fact status --> :Optimal
 
-  # FIXME what should those 3 values be ?
-  sosdec = SOSDecomposition(getslack(soscon))
+  q = getslack(soscon)
+  Q = getmat(q)
+  @fact issymmetric(Q) --> true
+  @fact isapprox(Q[1, 1], 2, rtol=1e-5) --> true
+  @fact isapprox(Q[1, 2], 1, rtol=1e-5) --> true
+  @fact isapprox(Q[3, 3], 5, rtol=1e-5) --> true
+  @fact abs(Q[2, 3]) < 1e-5 --> true
+  @fact isapprox(Q[2, 2] + 2Q[1, 3], -1, rtol=1e-5) --> true
+  sosdec = SOSDecomposition(q)
   @fact isapprox(sum(sosdec.ps.^2), p; rtol=1e-4) --> true
 
   M = Model(solver = solver)
