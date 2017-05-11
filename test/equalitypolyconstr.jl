a08601401882ca13f3d2ced40d5cdb2e0d876356
Joey Huchette <joehuchette@gmail.com>
2017-05-11 17:31:58 -0400

    Update test

--- test/equalitypolyconstr.jl
+++ test/equalitypolyconstr.jl
@@ -5,14 +5,7 @@
 
     @polyconstraint(m, x^3 + x*y^2 == x, domain=(x^2 + y^2 >= 1 && x^2 + y^2 <= 1))
 
-    # @variable(m, γ)
-    # @polyconstraint(m, γ >= x + y)
-
-    # @objective(m, Min, γ)
-
     status = solve(m)
 
     @test status == :Optimal || (iscsdp(solver) && status == :Suboptimal)
-
-    @test isapprox(getobjectivevalue(m), 3; rtol=1e-3)
 end
