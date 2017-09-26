de97b3368578a0d2403c51e3bc86aea50af397b8
Benoît Legat <benoit.legat@gmail.com>
2017-09-26 13:54:24 +0200

    Fix options pricing example

--- test/options_pricing.jl
+++ test/options_pricing.jl
@@ -9,32 +9,28 @@ using MultivariateMoments
     isscs(solver) && continue
     @polyvar x y z
     σ = [184.04, 164.88, 164.88, 184.04, 164.88, 184.04]
-    X2 = [x^2, x*y, x*z, y^2, y*z, z^2, x, y, z, 1]
-    X = [x, y, z, 1]
+    X = [x^2, x*y, x*z, y^2, y*z, z^2, x, y, z, 1]
     μ = Measure([σ + 44.21^2; 44.21 * ones(3); 1],
-                X2)
-    function optionspricing(K, poly, cone)
+                X)
+    function optionspricing(K, cone)
         m = SOSModel(solver = solver)
-        @variable m p poly(X)
+        @variable m p Poly(X)
+        @constraint m p in cone
         @constraint m p - (x - K) in cone
         @constraint m p - (y - K) in cone
         @constraint m p - (z - K) in cone
         @objective m Min dot(μ, p)
         status = solve(m)
         @test status == :Optimal
+        @show getobjectivevalue(m)
         getobjectivevalue(m)
     end
 
     K = [30, 35, 40, 45, 50]
-    #dsos_codsos_exp = [132.63, 132.63, 132.63, 132.63, 132.63]
-    dsos_codsos_exp = [1670.99, 1670.99, 1670.99, 1670.99, 1670.99]
-    dsos_cosos_exp = [21.73, 18.53, 16.16, 14.32, 12.86]
-    sos_codsos_exp = [265.50, 265.50, 265.50, 265.50, 265.50]
+    dsos_codsos_exp = [132.63, 132.63, 132.63, 132.63, 132.63]
     sos_cosos_exp = [21.51, 17.17, 13.20, 9.85, 7.30]
     for i in eachindex(K)
-        @test optionspricing(K[i], DSOSPoly, CoDSOSCone()) ≈ dsos_codsos_exp[i] atol=1e-2
-        @test optionspricing(K[i], DSOSPoly, CoSOSCone()) ≈ dsos_cosos_exp[i] atol=1e-2
-        @test optionspricing(K[i], SOSPoly, CoDSOSCone()) ≈ sos_codsos_exp[i] atol=1e-2
-        @test optionspricing(K[i], SOSPoly, CoSOSCone()) ≈ sos_cosos_exp[i] atol=1e-2
+        @test optionspricing(K[i], CoDSOSCone()) ≈ dsos_codsos_exp[i] atol=1e-2
+        @test optionspricing(K[i], CoSOSCone()) ≈ sos_cosos_exp[i] atol=1e-2
     end
 end
