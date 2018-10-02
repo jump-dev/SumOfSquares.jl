f8bf0ce3e2dd1513b8f1da8a498a9ffa5512e797
Benoît Legat <benoit.legat@gmail.com>
2018-10-02 12:26:09 +0200

    Update example notebook

--- test/solvers.jl
+++ test/solvers.jl
@@ -27,4 +27,4 @@ csd && push!(sdp_solvers, CSDP.CSDPSolver(printlevel=0))
 iscsdp(solver) = occursin("CSDP", string(typeof(solver)))
 # Need 54000 iterations for sosdemo3 to pass on Linux 64 bits
 # With 55000, sosdemo3 passes for every platform except Windows 64 bits on AppVeyor
-scs && push!(sdp_solvers, SCS.SCSSolver(eps=1e-6, max_iters=60000, verbose=0))
+scs && push!(sdp_solvers, SCS.SCSSolver(eps=1e-5, max_iters=60000, verbose=0))
