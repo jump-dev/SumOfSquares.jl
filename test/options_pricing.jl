2b872adf7cbe9d480b1d394d69b2fe1aad340c50
Benoît Legat <benoit.legat@gmail.com>
2018-11-18 10:11:29 +0100

    Add @testset in options_pricing.jl

--- test/options_pricing.jl
+++ test/options_pricing.jl
@@ -12,26 +12,29 @@ using MultivariateMoments
     X = [x^2, x*y, x*z, y^2, y*z, z^2, x, y, z, 1]
     μ = measure([σ .+ 44.21^2; 44.21 * ones(3); 1],
                 X)
-    function optionspricing(K, cone)
-        m = SOSModel(factory)
-        @variable m p Poly(X)
-        @constraint m p in cone
-        @constraint m p - (x - K) in cone
-        @constraint m p - (y - K) in cone
-        @constraint m p - (z - K) in cone
-        @objective m Min dot(μ, p)
-        JuMP.optimize!(m)
-        @test JuMP.primal_status(m) == MOI.FEASIBLE_POINT
-        JuMP.objective_value(m)
-    end
 
-    K = [30, 35, 40, 45, 50]
     dsos_codsos_exp   = [132.63, 132.63, 132.63, 132.63, 132.63]
     sdsos_cosdsos_exp = [ 21.51,  17.17,  13.20,   9.85,   7.30]
     sos_cosos_exp = sdsos_cosdsos_exp
-    for i in eachindex(K)
-        @test optionspricing(K[i], CoDSOSCone())  ≈ dsos_codsos_exp[i]   atol=1e-2
-        @test optionspricing(K[i], CoSDSOSCone()) ≈ sdsos_cosdsos_exp[i] atol=1e-2
-        @test optionspricing(K[i], CoSOSCone())   ≈ sos_cosos_exp[i]     atol=1e-2
+    @testset "with $cone" for (cone, exp) in [(CoDSOSCone(), dsos_codsos_exp),
+                                              (CoSDSOSCone(), sdsos_cosdsos_exp),
+                                              (CoSOSCone(), sdsos_cosdsos_exp)]
+        function optionspricing(K, cone)
+            m = SOSModel(factory)
+            @variable m p Poly(X)
+            @constraint m p in cone
+            @constraint m p - (x - K) in cone
+            @constraint m p - (y - K) in cone
+            @constraint m p - (z - K) in cone
+            @objective m Min dot(μ, p)
+            JuMP.optimize!(m)
+            @test JuMP.primal_status(m) == MOI.FEASIBLE_POINT
+            JuMP.objective_value(m)
+        end
+
+        K = [30, 35, 40, 45, 50]
+        for i in eachindex(K)
+            @test optionspricing(K[i], cone) ≈ exp[i] atol=1e-2
+        end
     end
 end
