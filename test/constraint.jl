2b6e2a4f5a54e73794208ddca280686656169a6a
Benoît Legat <benoit.legat@gmail.com>
2019-02-16 14:24:47 +0100

    Implement printing

--- test/constraint.jl
+++ test/constraint.jl
@@ -7,3 +7,12 @@ import PolyJuMP
     err = ErrorException("In `@constraint(model, [1 x; -x 0] in SOSMatrixCone())`: The polynomial matrix constrained to be SOS must be symmetric.")
     @test_throws err @constraint(model, [1 x; -x 0] in SOSMatrixCone())
 end
+
+@testset "Printing" begin
+    @polyvar x
+    model = SOSModel()
+    @variable(model, a)
+    cref = @constraint(model, a * x^2 >= 1)
+    @test sprint(show, MIME"text/plain"(), cref) == "(a)x² + (-1) ∈ SOSCone()"
+    @test sprint(show, MIME"text/latex"(), cref) == "\$ (a)x^{2} + (-1) \\in SOSCone() \$"
+end
