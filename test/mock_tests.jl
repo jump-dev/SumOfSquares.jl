2991e008a516b1c17986f93afa21e3c1ffeeec78
Benoît Legat <benoit.legat@gmail.com>
2019-02-24 16:42:17 +0100

    ✅ Only execute Term test on post MOI v0.8.2

--- test/mock_tests.jl
+++ test/mock_tests.jl
@@ -3,13 +3,16 @@ include("utilities.jl")
 
 using Test, JuMP
 
-@testset "Term" begin
-    config = MOI.Test.TestConfig()
-    optimize!(mock) = MOIU.mock_optimize!(mock, [0.0, 0.0],
-        (MOI.VectorAffineFunction{Float64}, MOI.Nonnegatives) => [[1.0]],
-        (MOI.VectorAffineFunction{Float64}, MOI.Zeros) => [[1.0]])
-    mock = bridged_mock(optimize!)
-    Tests.sos_term_test(mock, config)
-    Tests.sdsos_term_test(mock, config)
-    Tests.dsos_term_test(mock, config)
+# Needs https://github.com/JuliaOpt/MathOptInterface.jl/pull/669
+if Pkg.installed()["MathOptInterface"] > v"0.8.2"
+    @testset "Term" begin
+        config = MOI.Test.TestConfig()
+        optimize!(mock) = MOIU.mock_optimize!(mock, [0.0, 0.0],
+            (MOI.VectorAffineFunction{Float64}, MOI.Nonnegatives) => [[1.0]],
+            (MOI.VectorAffineFunction{Float64}, MOI.Zeros) => [[1.0]])
+        mock = bridged_mock(optimize!)
+        Tests.sos_term_test(mock, config)
+        Tests.sdsos_term_test(mock, config)
+        Tests.dsos_term_test(mock, config)
+    end
 end
