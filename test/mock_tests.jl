246fe43400a8ed24efe1b26a3dd8ebc0cb3890ec
Benoît Legat <benoit.legat@gmail.com>
2019-02-28 16:20:36 +0100

    Fix CI

--- test/mock_tests.jl
+++ test/mock_tests.jl
@@ -42,23 +42,25 @@ end
     mock = bridged_mock(optimize!)
     Tests.sos_concave_then_convex_cubic_test(mock, config)
 end
-@testset "Horn" begin
-    config = MOI.Test.TestConfig()
-    optimize!_inf(mock) = MOIU.mock_optimize!(mock, MOI.INFEASIBLE)
-    optimize!(mock) = MOIU.mock_optimize!(mock,
-        [1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0,
-         -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0,
-         1.0, -1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0,
-         1.0, 1.0, -1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0,
-         1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, 1.0,
-         1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 0.0, 0.0, 0.0,
-         0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0, 4.0, 4.0, 0.0, 0.0, 0.0, 0.0,
-         0.0, 0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-         0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-    mock = bridged_mock(optimize!_inf, optimize!_inf, optimize!)
-    Tests.sos_horn_test(mock, config)
-    mock = bridged_mock(optimize!_inf)
-    Tests.dsos_horn_test(mock, config)
-    mock = bridged_mock(optimize!_inf)
-    Tests.sdsos_horn_test(mock, config)
+if Pkg.installed()["MathOptInterface"] > v"0.8.2"
+    @testset "Horn" begin
+        config = MOI.Test.TestConfig()
+        optimize!_inf(mock) = MOIU.mock_optimize!(mock, MOI.INFEASIBLE)
+        optimize!(mock) = MOIU.mock_optimize!(mock,
+            [1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0,
+             -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0,
+             1.0, -1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0,
+             1.0, 1.0, -1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0,
+             1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, 1.0,
+             1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 0.0, 0.0, 0.0,
+             0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0, 4.0, 4.0, 0.0, 0.0, 0.0, 0.0,
+             0.0, 0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+             0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        mock = bridged_mock(optimize!_inf, optimize!_inf, optimize!)
+        Tests.sos_horn_test(mock, config)
+        mock = bridged_mock(optimize!_inf)
+        Tests.dsos_horn_test(mock, config)
+        mock = bridged_mock(optimize!_inf)
+        Tests.sdsos_horn_test(mock, config)
+    end
 end
