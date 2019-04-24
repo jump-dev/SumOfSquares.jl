99cbf4d198e046561ae60da080524a5e513435a6
Benoît Legat <benoit.legat@gmail.com>
2019-04-24 15:47:05 +0200

    Add missing prefix

--- test/sdsos_simple.jl
+++ test/sdsos_simple.jl
@@ -6,7 +6,7 @@
             @objective(model, Max, α)
             @polyvar x y
             @constraint(model, x^2 + α*x*y + y^2 in SDSOSCone())
-            optimize!(model)
+            JuMP.optimize!(model)
             @test JuMP.value(α) ≈ 2.0 rtol=1e-5
             @test JuMP.objective_value(model) ≈ 2.0 rtol=1e-5
         end
