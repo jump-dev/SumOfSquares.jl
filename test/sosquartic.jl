ccd3b592c7ade265c25becf98c28b1c57a085c9d
tweisser <tillmann.weisser@web.de>
2019-08-20 16:21:11 -0600

    example that 0:maxdegree_s is necessary

--- test/sosquartic.jl
+++ test/sosquartic.jl
@@ -1,17 +1,16 @@
  # Test quadratic module when mindegree of polynomial is strictly bigger than 0
 
 @testset "SOSquartic with $(factory.constructor)" for factory in sdp_factories
-    @polyvar x  
+    @polyvar x y
     m = SOSModel(factory)
-    p = x^2*(2-x^2)
-    S = @set 1-x^2 >= 0 && x^2 - 0.5 >=0
+    p = x^4 - y^4 
+    S = @set - x^2 - y^4 >=0
     soscon = @constraint m p >= 0 domain = S 
 
     JuMP.optimize!(m)
        
     @test JuMP.primal_status(m) == MOI.FEASIBLE_POINT
     Q = gram_matrix(soscon)
-    @test mindegree(monomials(Q)) == 0
-    @test maxdegree(monomials(Q)) == 4
-
+    @test mindegree(monomials(Q)) == 2
+    @test mindegree(monomials(Q)) == 4
 end
