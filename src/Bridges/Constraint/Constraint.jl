f1eaf886ea6177572ced0a6556a878cee477fbf1
Benoît Legat <benoit.legat@gmail.com>
2019-09-02 13:49:25 +0200

    Move and rename bridges

--- src/Bridges/Constraint/Constraint.jl
+++ src/Bridges/Constraint/Constraint.jl
@@ -1,12 +1,13 @@
 module Constraint
 
 # Symmetric PSD matrix bridges
-include("psd2x2_bridge.jl")
-include("diagonally_dominant_bridge.jl")
-include("scaled_diagonally_dominant_bridge.jl")
+include("empty.jl")
+include("psd2x2.jl")
+include("diagonally_dominant.jl")
+include("scaled_diagonally_dominant.jl")
 
 # SOS polynomial bridges
-include("sos_polynomial_bridge.jl")
-include("sos_polynomial_in_semialgebraic_set_bridge.jl")
+include("sos_polynomial.jl")
+include("sos_polynomial_in_semialgebraic_set.jl")
 
 end
