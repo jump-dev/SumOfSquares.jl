e3698b60652a82fbcf73d24bc08336816d4c29d6
tweisser <tillmann.weisser@web.de>
2019-09-05 09:41:57 -0600

    test chordal_sos

--- src/csp/test_sparse_putinar.jl
+++ src/csp/test_sparse_putinar.jl
@@ -4,21 +4,27 @@ using MosekTools
 
 using Test
 
-include("test_functions")
+include("test_functions.jl")
 
-@testset chained_singular begin
+@testset "chained_singular" begin
+    n = 100
+    m = SOSModel(with_optimizer(Mosek.Optimizer))
+    @variable m t
+    @objective m Max t
+    chordal_sos(chained_singular(n)-t; model = m)
+    optimize!(m)
 end
 
-@testset broyden_banded begin
+@testset "broyden_banded" begin
 end
 
-@testset broyden_tridiagonal begin
+@testset "broyden_tridiagonal" begin
 end
 
-@testset chained_wood begin
+@testset "chained_wood" begin
 end
 
-@testset generalized_rosenbrock begin
+@testset "generalized_rosenbrock" begin
 end
 
 
