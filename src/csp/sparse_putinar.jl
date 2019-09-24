9901ad5ab323b9141c92a5724fbca1c2534d7289
tweisser <tillmann.weisser@web.de>
2019-09-24 11:43:07 -0600

    remove dependency from DynamicPolynomials

--- src/csp/sparse_putinar.jl
+++ src/csp/sparse_putinar.jl
@@ -18,9 +18,13 @@ function chordal_sos(p::APL; model::JuMP.Model = SOSModel())
     return model
 end
 
-using DynamicPolynomials
 """
-chordal_putinar(p::T, degree::Int; equalities = T[], inequalities = T[],  model = SOSModel)
+    chordal_putinar(
+                         p::APL, 
+                         degree::Int,
+                         K::AbstractBasicSemialgebraicSet;
+                         model::JuMP.Model = SOSModel()
+                        )
 
 
 """
@@ -34,51 +38,38 @@ function chordal_putinar(
     if K isa FullSpace
         println("Unbounded domain. Ignoring degree = $degree .")
         model = chordal_sos(p, model)
-    elseif K isa AbstractAlgebraicSet
-        H, cliques = chordal_csp_graph(p, equalities(K))
+    else
+        H, cliques = chordal_csp_graph(p, K)
+
         for clique in cliques
+
             vars = Tuple(unique!(sort!(clique, rev = true)))
             mvec = MP.monomials(vars, 0:MP.maxdegree(p))
             pp = @variable model variable_type=Poly(mvec) 
+
             p = p - pp
+
+
+            Ki = BasicSemialgebraicSet{Float64, typeof(p)}()
+            if K isa BasicSemiAlgebraicSet
+                for ineq in inequalities(K)
+                    if CEG.contains(clique, variables(ineq))
+                        addinequality!(Ki,ineq)
+                    end
+                end
+            end
+
             for eq in equalities(K)
                 if CEG.contains(clique, variables(eq))
                     deg = degree - MP.maxdegree(eq)
                     if deg >= 0
                         mvec = MP.monomials(vars, 0:deg)
                         ppi = @variable model variable_type=Poly(mvec)
-                        pp = pp - ppi[1]*eq
+                        pp = pp - ppi*eq
                     end
                 end
             end
-            @constraint model pp in SOSCone()
-        end
-        @constraint model p ==0
-    else
-        if !isempty(equalities(K))
-            constraints =  [equalities(K)..., inequalities(K)...]
-        else
-            constraints = inequalities(K)
-        end
-        
-        H, cliques = chordal_csp_graph(p,constraints)
 
-        for clique in cliques
-            Ki = BasicSemialgebraicSet{Float64, DynamicPolynomials.Polynomial{true,Float64}}()
-            vars = Tuple(unique!(sort!(clique, rev = true)))
-            mvec = MP.monomials(vars, 0:MP.maxdegree(p))
-            pp = @variable model variable_type=Poly(mvec) 
-            p = p - pp
-            for eq in equalities(K)
-                if CEG.contains(clique, variables(eq))
-                    addequality!(Ki, eq)                    
-                end
-            end
-            for ineq in inequalities(K)
-                if CEG.contains(clique, variables(ineq))
-                    addinequality!(Ki,ineq)
-                end
-            end
             @constraint model pp in SOSCone() domain = Ki maxdegree = degree
         end
 
