4152985fd514be76a9561065e2762aca6bdcd22a
Benoît Legat <benoit.legat@gmail.com>
2019-10-14 13:00:05 +0200

    Update solver tests

--- test/Solvers/mosek_tests.jl
+++ test/Solvers/mosek_tests.jl
@@ -9,8 +9,17 @@ config = MOI.Test.TestConfig(atol=1e-5, rtol=1e-5)
     Tests.linear_test(factory, config)
 end
 @testset "SOC" begin
-    Tests.soc_test(factory, config)
+    Tests.soc_test(factory, config, [
+        # FIXME Mosek.MosekError(1307, "Variable '' (29) is a member of cone '' (9).")
+        "sdsos_horn"
+    ])
 end
 @testset "SDP" begin
-    Tests.sd_test(factory, config)
+    Tests.sd_test(factory, config, [
+        # FIXME AssertionError: m.x_type[ref2id(vi)] == Deleted
+        "maxcut", "sos_horn", "motzkin",
+        # Expression: JuMP.termination_status(model) == MOI.INFEASIBLE
+        # Evaluated: MathOptInterface.SLOW_PROGRESS == MathOptInterface.INFEASIBLE
+        "quartic_infeasible_lyapunov_switched_system"
+    ])
 end
--- test/Solvers/sdpa_tests.jl
+++ test/Solvers/sdpa_tests.jl
@@ -3,22 +3,40 @@ import SDPA
 factory = with_optimizer(SDPA.Optimizer)
 config = MOI.Test.TestConfig(atol=1e-5, rtol=1e-5, query=false)
 @testset "Linear" begin
-    # With `dsos_horn_test`, the termination status is
-    # `INFEASIBLE_OR_UNBOUNDED` instead of `INFEASIBLE`.
-    # With `dsos_concave_then_convex_cubic`, we have
-    # cholesky miss condition :: not positive definite :: info = 32 :: line 785 in sdpa_linear.cpp
-    # There are some possibilities. :: line 786 in sdpa_linear.cpp
-    # 1. SDPA terminates due to inaccuracy of numerical error :: line 787 in sdpa_linear.cpp
-    # 2. The input problem may not have (any) interior-points :: line 788 in sdpa_linear.cpp
-    # 3. Input matrices are linearly dependent :: line 789 in sdpa_linear.cpp
-    Tests.linear_test(factory, config,
-                      ["dsos_horn", "dsos_concave_then_convex_cubic"])
+    Tests.linear_test(factory, config, [
+        # The termination status is
+        # `INFEASIBLE_OR_UNBOUNDED` instead of `INFEASIBLE`.
+        "dsos_horn",
+        # cholesky miss condition :: not positive definite :: info = 32 :: line 785 in sdpa_linear.cpp
+        # There are some possibilities. :: line 786 in sdpa_linear.cpp
+        # 1. SDPA terminates due to inaccuracy of numerical error :: line 787 in sdpa_linear.cpp
+        # 2. The input problem may not have (any) interior-points :: line 788 in sdpa_linear.cpp
+        # 3. Input matrices are linearly dependent :: line 789 in sdpa_linear.cpp
+        "dsos_concave_then_convex_cubic",
+        # The termination status is
+        # `SLOW_PROGRESS` instead of `INFEASIBLE`.
+        # cholesky miss condition :: not positive definite :: info = 7 :: line 785 in sdpa_linear.cpp
+        # There are some possibilities. :: line 786 in sdpa_linear.cpp
+        # 1. SDPA terminates due to inaccuracy of numerical error :: line 787 in sdpa_linear.cpp
+        # 2. The input problem may not have (any) interior-points :: line 788 in sdpa_linear.cpp
+        # 3. Input matrices are linearly dependent :: line 789 in sdpa_linear.cpp
+        "dsos_bivariate_quadratic", "dsos_univariate_quadratic"])
 end
 @testset "SDP" begin
     # With `sos_concave_then_convex_cubic`, we have the same error than with
     # `dsos_concave_then_convex_cubic`.
-    Tests.sd_test(factory, config,
-                  ["sos_concave_then_convex_cubic",
-                   "quadratic_infeasible_lyapunov_switched_system",
-                   "quartic_infeasible_lyapunov_switched_system"])
+    Tests.sd_test(factory, config, [
+        "sos_concave_then_convex_cubic",
+        "quartic_infeasible_lyapunov_switched_system",
+        # Strange behavior : primal < dual :: line 144 in sdpa_solve.cpp
+        # pdINF criteria :: line 1192 in sdpa_parts.cpp
+        "maxcut",
+        # Strange behavior : primal < dual :: line 144 in sdpa_solve.cpp
+        # dUNBD criteria :: line 1220 in sdpa_parts.cpp
+        # dUNBD criteria :: line 1220 in sdpa_parts.cpp
+        # dUNBD criteria :: line 1220 in sdpa_parts.cpp
+        # Step length is too small.  :: line 198 in sdpa_dataset.cpp
+        # cannot move: step length is too short :: line 176 in sdpa_solve.cpp
+        "sos_horn"
+    ])
 end
