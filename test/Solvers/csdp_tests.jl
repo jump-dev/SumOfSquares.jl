ee0dfc5c0a8a941bd8c2d2a44be7f995428469b0
Benoît Legat <benoit.legat@gmail.com>
2019-10-21 16:07:45 +0200

    Fix CSDP segfault

--- test/Solvers/csdp_tests.jl
+++ test/Solvers/csdp_tests.jl
@@ -3,10 +3,7 @@ import CSDP
 factory = with_optimizer(CSDP.Optimizer, printlevel=0)
 config = MOI.Test.TestConfig(atol=1e-4, rtol=1e-4, query=false)
 @testset "Linear" begin
-    Tests.linear_test(factory, config, [
-        # Segfaults, see https://github.com/JuliaOpt/CSDP.jl/issues/39
-        "dsos_horn",
-    ])
+    Tests.linear_test(factory, config)
 end
 @testset "SDP" begin
     Tests.sd_test(factory, config)
