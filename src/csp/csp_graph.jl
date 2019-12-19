a35e96a48759682dcff665d5f05ad343de428a91
Benoît Legat <benoit.legat@gmail.com>
2019-12-19 21:13:37 +0100

    Use MP.effective_variables and MP.variable_union_type

--- src/csp/csp_graph.jl
+++ src/csp/csp_graph.jl
@@ -11,17 +11,8 @@ function effective_variables(p::APL)
     return ev
 end
 
-function JuMP.variable_type(v::Vector{<:APL})
-    vararr = [i for i in variables(sum(vi for vi in v))]
-    return eltype(vararr)
-end
-
-function JuMP.variable_type(p::APL)
-    return variable_type([p])
-end
 function csp_ojective_cliques(p::APL)
-    mv = MP.monomials(p)
-    return [effective_variables(m) for m in mv]
+    return map(effective_variables, MP.monomials(p))
 end
 
 function csp_constraint_clique(p::APL)
@@ -34,7 +25,7 @@ function csp_graph(::Type{T}, objective::APL, constraints::Vector{<:APL}, G::CEG
     return G
 end
 
-function csp_graph(objective::APL, constraints::Vector{<:APL}, G::CEG.LabelledGraph = CEG.LabelledGraph{variable_type(objective)}() )
+function csp_graph(objective::APL, constraints::Vector{<:APL}, G::CEG.LabelledGraph = CEG.LabelledGraph{MP.variable_union_type(objective)}() )
     CEG.add_clique!.(G, csp_ojective_cliques(objective))
     CEG.add_clique!.(G, csp_constraint_clique.(constraints))
     return G
@@ -44,15 +35,15 @@ function chordal_csp_graph(::Type{T}, objective::APL, constraints::Vector{<:APL}
     return CEG.chordal_extension(csp_graph(T, objective, constraints, G), CEG.GreedyFillIn())
 end
 
-function chordal_csp_graph(objective::APL, constraints::Vector{<:APL}, G::CEG.LabelledGraph = CEG.LabelledGraph{variable_type(objective)}() )
+function chordal_csp_graph(objective::APL, constraints::Vector{<:APL}, G::CEG.LabelledGraph = CEG.LabelledGraph{MP.variable_union_type(objective)}() )
     return CEG.chordal_extension(csp_graph(objective, constraints, G), CEG.GreedyFillIn())
 end
 
-function chordal_csp_graph(objective::APL, G::CEG.LabelledGraph = CEG.LabelledGraph{variable_type(objective)}() )
+function chordal_csp_graph(objective::APL, G::CEG.LabelledGraph = CEG.LabelledGraph{MP.variable_union_type(objective)}() )
     return CEG.chordal_extension(csp_graph(objective, typeof(objective)[], G), CEG.GreedyFillIn())
 end
 
-function chordal_csp_graph(objective::APL, K::AbstractBasicSemialgebraicSet, G::CEG.LabelledGraph = CEG.LabelledGraph{variable_type(objective)}() )
+function chordal_csp_graph(objective::APL, K::AbstractBasicSemialgebraicSet, G::CEG.LabelledGraph = CEG.LabelledGraph{MP.variable_union_type(objective)}() )
     constraints = Vector{typeof(objective)}()
     if K isa BasicSemialgebraicSet
         append!(constraints, inequalities(K))
