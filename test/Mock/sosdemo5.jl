17929aba1468ce0c47fe207482c29443082302f7
Benoît Legat <benoit.legat@gmail.com>
2019-12-22 13:28:55 +0100

    Make sosdemo5 solver-independent and test with mock

---
+++ test/Mock/sosdemo5.jl
@@ -0,0 +1,10 @@
+config = MOI.Test.TestConfig()
+optimize!(mock) = MOIU.mock_optimize!(mock, MOI.INFEASIBLE, tuple(), MOI.INFEASIBILITY_CERTIFICATE)
+for mock in mocks(optimize!)
+    Tests.sosdemo5_infeasible_test(mock, config)
+end
+# The test does not check the solution so we just set zeros.
+optimize!(mock) = MOIU.mock_optimize!(mock, zeros(MOI.get(mock, MOI.NumberOfVariables())))
+for mock in mocks(optimize!)
+    Tests.sosdemo5_feasible_test(mock, config)
+end
--- test/Tests/sosdemo5.jl
+++ test/Tests/sosdemo5.jl
@@ -2,8 +2,9 @@
 # SOSDEMO5 --- Upper bound for the structured singular value mu
 # Section 3.5 of SOSTOOLS User's Manual
 
-@testset "SOSDEMO5 with $(factory.constructor)" for factory in sdp_factories
-    isscs(factory) && continue
+using SparseArrays
+
+function sosdemo5_test(optimizer, config::MOIT.TestConfig, feasible::Bool, γ)
     @polyvar x[1:8]
 
     # The matrix under consideration
@@ -18,53 +19,57 @@
     V = [0 a; b -b; c -im*c; -im*f -d];
     M = U*V';
 
-    @testset "with γ=$γ it should be $(feasible ? "feasible" : "infeasible")" for (γ, feasible) in ((0.8722, false), (0.8724, true))
-        Z = monomials(x, 1)
+    Z = monomials(x, 1)
 
-        function build_A(i)
-            H = M[i,:]*M[i,:]' - (γ^2)*sparse([i],[i],[1],4,4)
-            H = [real(H) -imag(H); imag(H) real(H)]
-            dot(Z, H*Z)
-        end
-        A = build_A.(1:4)
+    function build_A(i)
+        H = M[i,:]*M[i,:]' - (γ^2)*sparse([i],[i],[1],4,4)
+        H = [real(H) -imag(H); imag(H) real(H)]
+        dot(Z, H*Z)
+    end
+    A = build_A.(1:4)
 
-        m = SOSModel(factory)
+    model = _model(optimizer)
+    PolyJuMP.setpolymodule!(model, SumOfSquares)
 
-        # -- Q(x)'s -- : sums of squares
-        # Monomial vector: [x1; ... x8]
-        Q = Vector{GramMatrix{JuMP.VariableRef, monomialtype(x[1]),
-                                 monovectype(x[1])}}(undef, 4)
-        @variable m Q[1:4] SOSPoly(Z)
+    # -- Q(x)'s -- : sums of squares
+    # Monomial vector: [x1; ... x8]
+    Q = Vector{GramMatrix{JuMP.VariableRef, monomialtype(x[1]),
+                          monovectype(x[1])}}(undef, 4)
+    @variable model Q[1:4] SOSPoly(Z)
 
-        # -- r's -- : constant sum of squares
-        Z = monomials(x, 0)
-        #r = Matrix{GramMatrix{JuMP.VariableRef}}(4,4) # FIXME doesn't work with 1x1 SDP matrix :(
-        @variable m r[i=1:4,j=(i+1):4] >= 0
+    # -- r's -- : constant sum of squares
+    Z = monomials(x, 0)
+    #r = Matrix{GramMatrix{JuMP.VariableRef}}(4,4) # FIXME doesn't work with 1x1 SDP matrix :(
+    @variable model r[i=1:4,j=(i+1):4] >= 0
 
-        # Constraint : -sum(Qi(x)*Ai(x)) - sum(rij*Ai(x)*Aj(x)) + I(x) >= 0
-        expr = 0
-        # Adding term
-        for i = 1:4
-            expr -= A[i]*Q[i]
-        end
-        for i = 1:4
-            for j = (i+1):4
-                expr -= A[i]*A[j]*r[i,j]
-            end
-        end
-        # Constant term: I(x) = -(x1^4 + ... + x8^4)
-        I = -sum(x.^4)
-        expr = expr + I
-
-        @constraint m expr >= 0
-
-        JuMP.optimize!(m)
-
-        # Program is feasible, thus 0.8724 is an upper bound for mu.
-        if feasible
-            @test JuMP.primal_status(m) == MOI.FEASIBLE_POINT
-        else
-            @test JuMP.dual_status(m) == MOI.INFEASIBILITY_CERTIFICATE
+    # Constraint : -sum(Qi(x)*Ai(x)) - sum(rij*Ai(x)*Aj(x)) + I(x) >= 0
+    expr = 0
+    # Adding term
+    for i = 1:4
+        expr -= A[i]*Q[i]
+    end
+    for i = 1:4
+        for j = (i+1):4
+            expr -= A[i]*A[j]*r[i,j]
         end
     end
+    # Constant term: I(x) = -(x1^4 + ... + x8^4)
+    I = -sum(x.^4)
+    expr = expr + I
+
+    @constraint model expr >= 0
+
+    JuMP.optimize!(model)
+
+    # Program is feasible, thus 0.8724 is an upper bound for mu.
+    if feasible
+        @test JuMP.primal_status(model) == MOI.FEASIBLE_POINT
+    else
+        @test JuMP.dual_status(model) == MOI.INFEASIBILITY_CERTIFICATE
+    end
 end
+const γ_opt = 0.8723
+sosdemo5_infeasible_test(optimizer, config, ε = 1e-2) = sosdemo5_test(optimizer, config, false, γ_opt - ε)
+sosdemo5_feasible_test(optimizer, config, ε = 1e-2) = sosdemo5_test(optimizer, config, true, γ_opt + ε)
+sd_tests["sosdemo5_infeasible"] = sosdemo5_infeasible_test
+sd_tests["sosdemo5_feasible"] = sosdemo5_feasible_test
