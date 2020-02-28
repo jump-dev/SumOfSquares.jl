33731b10cd7d1eb829a4a6f6e47c832f0e2375b7
Benoît Legat <benoit.legat@gmail.com>
2020-02-28 11:27:05 +0100

    Add missing file

---
+++ test/Mock/simple_matrix.jl
@@ -0,0 +1,6 @@
+config = MOI.Test.TestConfig()
+# The test does not check the solution so we just set zeros.
+optimize!(mock) = MOIU.mock_optimize!(mock, zeros(MOI.get(mock, MOI.NumberOfVariables())))
+for mock in mocks(optimize!)
+    Tests.simple_matrix_test(mock, config)
+end
