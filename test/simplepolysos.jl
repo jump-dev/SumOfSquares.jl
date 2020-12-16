3386f37898837ceab45c8360826b3327bb73edcb
Benoît Legat <benoit.legat@gmail.com>
2020-12-16 13:09:16 +0100

    Replace Example 3.25 of [BPT12] by SOS decomposition example

--- test/simplepolysos.jl
+++ test/simplepolysos.jl
@@ -1,13 +1,6 @@
-# Example 3.25, 3.35 and 3.38 of
+# Example 3.35 and 3.38 of
 # Blekherman, G., Parrilo, P. A., & Thomas, R. R. (Eds.).
 # Semidefinite optimization and convex algebraic geometry SIAM 2013
-@testset "Example 3.25 with $(factory.optimizer_constructor)" for factory in sdp_factories
-    @polyvar x y
-    m = SOSModel(factory)
-    @constraint m x^2-x*y^2+y^4+1 >= 0
-    JuMP.optimize!(m)
-    @test JuMP.primal_status(m) == MOI.FEASIBLE_POINT
-end
 @testset "Example 3.35 with $(factory.optimizer_constructor)" for factory in sdp_factories
     @polyvar x
     m = SOSModel(factory)
