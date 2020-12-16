f12be7c1988587695406ee81e2e29ed427425c0b
Documenter.jl <documenter@juliadocs.github.io>
2020-12-16 10:15:52 +0000

    build based on fac5686

---
+++ previews/PR181/generated/Motzkin.jl
@@ -0,0 +1,51 @@
+using DynamicPolynomials
+@polyvar x y
+motzkin = x^4*y^2 + x^2*y^4 + 1 - 3x^2*y^2
+
+using SumOfSquares
+import CSDP
+solver = optimizer_with_attributes(CSDP.Optimizer, MOI.Silent() => true)
+model = SOSModel(solver)
+@constraint(model, motzkin >= 0) # We constraint `motzkin` to be a sum of squares
+
+optimize!(model)
+
+termination_status(model)
+
+dual_status(model)
+
+model = SOSModel(solver)
+@constraint(model, (x^2 + y^2) * motzkin >= 0) # We constraint the `(x^2 + y^2) * motzkin` to be a sum of squares
+
+optimize!(model)
+
+termination_status(model)
+
+primal_status(model)
+
+model = SOSModel(solver)
+X = monomials([x, y], 0:2)
+
+@variable(model, deno, Poly(X))
+
+@constraint(model, deno >= 1)
+@constraint(model, deno * motzkin >= 0)
+optimize!(model)
+
+termination_status(model)
+
+primal_status(model)
+
+value(deno)
+
+using RecipesBase
+using MultivariatePolynomials
+@recipe function f(x::AbstractVector, y::AbstractVector, p::Polynomial)
+    x, y, (x, y) -> p(variables(p) => [x, y])
+end
+using Plots
+plot(range(-2, stop=2, length=100), range(-2, stop=2, length=100), motzkin,
+     st = [:surface], seriescolor=:heat, colorbar=:none, clims = (-10, 80))
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
