2d9770d103ef0b8e5af0b2541146f5f565041492
Documenter.jl <documenter@juliadocs.github.io>
2021-02-25 22:50:35 +0000

    build based on dfe41ba

--- dev/generated/Symmetry reduction.jl
+++ dev/generated/Symmetry reduction.jl
@@ -1,5 +1,5 @@
 using Pkg
-pkg"add https://github.com/kalmarek/SymbolicWedderburn.jl#tw/ex_sos"
+pkg"add https://github.com/kalmarek/SymbolicWedderburn.jl"
 
 import MutableArithmetics
 const MA = MutableArithmetics
@@ -18,54 +18,19 @@ using PermutationGroups
 using Cyclotomics
 using SumOfSquares
 
-function SymbolicWedderburn.OnPoints(basis::Union{MonomialVector, AbstractVector{<:Monomial}})
-    basis_exps = Vector{Vector{Int}}(undef, length(basis))
+function SymbolicWedderburn.ExtensionHomomorphism(basis::MB.MonomialBasis)
+    monos = basis.monomials
+    basis_exps = Vector{Vector{Int}}(undef, length(monos))
     basis_dict = Dict{Vector{Int}, Int}()
-    sizehint!(basis_dict, length(basis))
+    sizehint!(basis_dict, length(monos))
 
-    for (i, b) in enumerate(basis)
+    for (i, b) in enumerate(monos)
         e = MP.exponents(b) # so that we allocate exponents only once
         basis_exps[i] = e
         basis_dict[e] = i
     end
 
-    return SymbolicWedderburn.OnPoints(basis_exps, basis_dict)
-end
-function symmetry_adapted_basis(G, mvec)
-    chars_vars = SymbolicWedderburn.characters_dixon(G)
-
-    chars = chars_vars
-    basis = mvec
-
-    @assert all(χ.inv_of == first(chars).inv_of for χ in chars)
-
-    induced_action = SymbolicWedderburn.OnPoints(basis)
-    ccG_large = induced_action.(conjugacy_classes(first(chars)))
-
-    let ccls = ccG_large, large_gens = induced_action.(gens(G)) # double check:
-        G_large = PermGroup(large_gens)
-        ccG_large = conjugacy_classes(G_large)
-        @assert all(Set.(collect.(ccG_large)) .== Set.(collect.(ccls)))
-    end
-
-    chars_mvec = [SymbolicWedderburn.Character(values(χ), χ.inv_of, ccG_large) for χ in chars]
-
-    vr_chars = SymbolicWedderburn.real_vchars(chars_mvec)
-    U = filter!(x->all(!iszero, x), [SymbolicWedderburn.matrix_projection(χ) for χ in vr_chars])
-    R = map(U) do c_u
-        u = last(c_u)
-        if all(isreal, u)
-            image_coeffs, pivots = SymbolicWedderburn.row_echelon_form(float.(u))
-            dim = length(pivots)
-            image_coeffs[1:dim, :]
-        else
-            throw("Not Implemented")
-        end
-    end
-
-    return map(R) do Ri
-        FixedPolynomialBasis(Ri * mvec)
-    end
+    return SymbolicWedderburn.ExtensionHomomorphism(basis_exps, basis_dict)
 end
 function MP.polynomialtype(::Type{<:MB.AbstractPolynomialVectorBasis{PT}}, T::Type) where PT
     C = MP.coefficienttype(PT)
@@ -84,7 +49,10 @@ Certificate.zero_basis(::SymmetricIdeal) = MB.MonomialBasis
 Certificate.get(::SymmetricIdeal, ::Certificate.ReducedPolynomial, poly, domain) = poly
 function Certificate.get(cert::SymmetricIdeal, ::Certificate.GramBasis, poly)
     basis = Certificate.maxdegree_gram_basis(MB.MonomialBasis, MP.variables(poly), MP.maxdegree(poly))
-    return symmetry_adapted_basis(cert.group, basis.monomials)
+    R = SymbolicWedderburn.symmetry_adapted_basis(cert.group, basis)
+    return map(R) do Ri
+        FixedPolynomialBasis(convert(Matrix{Float64}, Ri) * basis.monomials)
+    end
 end
 G = PermGroup([perm"(1,2,3,4)"])
 
@@ -93,7 +61,7 @@ solver = CSDP.Optimizer
 model = Model(solver)
 @variable(model, t)
 @objective(model, Max, t)
-con_ref = @constraint(model, sum(x) + sum(x.^2) - t in SOSCone(), ideal_certificate = SymmetricIdeal(SOSCone(), G))
+con_ref = @constraint(model, poly - t in SOSCone(), ideal_certificate = SymmetricIdeal(SOSCone(), G))
 optimize!(model)
 value(t)
 
