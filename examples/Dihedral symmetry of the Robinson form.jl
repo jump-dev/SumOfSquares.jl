c59c5313c4591c75b3eb192550ff9fd4540d00e9
Benoît Legat <benoit.legat@gmail.com>
2021-03-04 13:32:35 +0100

    Add alternative implemention of Dihedral group

--- examples/Dihedral symmetry of the Robinson form.jl
+++ examples/Dihedral symmetry of the Robinson form.jl
@@ -51,10 +51,13 @@ function Base.inv(el::DihedralElement)
         return DihedralElement(el.n, false, el.n - el.id)
     end
 end
-function PermutationGroups.mul!(::DihedralElement, a::DihedralElement, b::DihedralElement)
+function Base.:*(a::DihedralElement, b::DihedralElement)
     a.n == b.n || error("Cannot multiply elements from different Dihedral groups")
     id = mod(a.reflection ? a.id - b.id : a.id + b.id, a.n)
     return DihedralElement(a.n, a.reflection != b.reflection, id)
+end
+function PermutationGroups.mul!(::DihedralElement, a::DihedralElement, b::DihedralElement)
+    return a * b
 end
 function Base.:^(el::DihedralElement, k::Integer)
     if el.reflection
@@ -64,32 +67,26 @@ function Base.:^(el::DihedralElement, k::Integer)
     end
 end
 
+Base.conj(a::DihedralElement, b::DihedralElement) = inv(b) * a * b
+Base.:^(a::DihedralElement, b::DihedralElement) = conj(a, b)
+
 struct DihedralGroup <: Group
     n::Int
 end
-_orbit(cc::Vector{<:GroupElem}) = PermutationGroups.Orbit(cc, Dict(a => nothing for a in cc))
-_orbit(el::GroupElem) = _orbit([el])
-function SymbolicWedderburn.conjugacy_classes_orbit(d::DihedralGroup)
-    orbits = [_orbit(DihedralElement(d.n, false, 0))]
-    for i in 1:div(d.n - 1, 2)
-        push!(orbits, _orbit([
-            DihedralElement(d.n, false, i),
-            DihedralElement(d.n, false, d.n - i),
-        ]))
-    end
-    if iseven(d.n)
-        push!(orbits, _orbit(DihedralElement(d.n, false, div(d.n, 2))))
-        push!(orbits, _orbit([
-            DihedralElement(d.n, true, i) for i in 0:2:(d.n - 2)
-        ]))
-        push!(orbits, _orbit([
-            DihedralElement(d.n, true, i) for i in 1:2:(d.n - 1)
-        ]))
+Base.one(G::DihedralGroup) = DihedralElement(G.n, false, 0)
+PermutationGroups.gens(G::DihedralGroup) = [DihedralElement(G.n, false, 1), DihedralElement(G.n, true, 0)]
+PermutationGroups.order(::Type{T}, G::DihedralGroup) where {T} = convert(T, 2G.n)
+function Base.iterate(G::DihedralGroup, prev::DihedralElement=DihedralElement(G.n, false, -1))
+    if prev.id + 1 >= G.n
+        if prev.reflection
+            return nothing
+        else
+            next = DihedralElement(G.n, true, 0)
+        end
     else
-        push!(orbits, _orbit([
-            DihedralElement(d.n, true, i) for i in 0:(d.n - 1)
-        ]))
+        next = DihedralElement(G.n, prev.reflection, prev.id + 1)
     end
+    return next, next
 end
 
 # The Robinson form is invariant under the following action of the Dihedral group on monomials:
@@ -123,50 +120,98 @@ end
 
 poly = x^6 + y^6 - x^4 * y^2 - y^4 * x^2 - x^4 - y^4 - x^2 - y^2 + 3x^2 * y^2 + 1
 
-# We can verify that `poly` is indeed invariant under each element of the group as follows.
+# We can verify that `poly` is indeed invariant under the action of each element of the group as follows.
 
 G = DihedralGroup(4)
-for cc in SymbolicWedderburn.conjugacy_classes_orbit(G)
-    for g in cc
-        @show action(poly, g)
-        @show action(poly, g) == poly
-    end
+for g in G
+    @show action(poly, g)
+    @test action(poly, g) == poly #src
 end
 
 # We can exploit this symmetry for reducing the problem using the `SymmetricIdeal` certificate as follows:
 
 import CSDP
-solver = CSDP.Optimizer
-model = Model(solver)
-@variable(model, t)
-@objective(model, Max, t)
-con_ref = @constraint(model, poly - t in SOSCone(), ideal_certificate = SymmetricIdeal(SOSCone(), G, action))
-optimize!(model)
-@test value(t) ≈ -3825/4096 rtol=1e-3 #src
-value(t)
+function solve(G)
+    solver = CSDP.Optimizer
+    model = Model(solver)
+    @variable(model, t)
+    @objective(model, Max, t)
+    con_ref = @constraint(model, poly - t in SOSCone(), ideal_certificate = SymmetricIdeal(SOSCone(), G, action))
+    optimize!(model)
+    @test value(t) ≈ -3825/4096 rtol=1e-3 #src
+    @show value(t)
 
-# We indeed find `-3825/4096`, let's verify that symmetry was exploited:
 
-g = gram_matrix(con_ref).sub_gram_matrices #src
-@test length(g) == 4                       #src
-@test g[1].basis.polynomials == [x^3, x^2*y, x*y^2, y^3, x, y] #src
-for I in [[5, 1, 3], [6, 4, 2]]            #src
-    Q = g[1].Q[I, I]                       #src
-    @test Q[1, 1] ≈ 25/64 rtol=1e-3 #src
-    @test Q[1, 2] ≈ -5/8  rtol=1e-3 #src
-    @test Q[1, 3] ≈  5/8  rtol=1e-3 #src
-    @test Q[2, 2] ≈  1    rtol=1e-3 #src
-    @test Q[2, 3] ≈ -1    rtol=1e-3 #src
-    @test Q[3, 3] ≈  1    rtol=1e-3 #src
-end #src
-@test g[2].basis.polynomials == [x^2 + y^2, 1.0] #src
-@test g[2].Q[2, 2] ≈ 7921/4096 rtol=1e-3 #src
-@test g[2].Q[1, 2] ≈  -89/128 rtol=1e-3  #src
-@test g[2].Q[1, 1] ≈ 1/4 rtol=1e-3 #src
-@test g[3].basis.polynomials == [x * y] #src
-@test g[3].Q[1, 1] ≈ 0   atol=1e-3 #src
-@test g[4].basis.polynomials == [x^2 - y^2] #src
-@test g[4].Q[1, 1] ≈ 0   atol=1e-3 #src
-for g in gram_matrix(con_ref).sub_gram_matrices
-    println(g.basis.polynomials)
+    g = gram_matrix(con_ref).sub_gram_matrices #src
+    @test length(g) == 4                       #src
+    @test g[1].basis.polynomials == [x^3, x^2*y, x*y^2, y^3, x, y] #src
+    for I in [[5, 1, 3], [6, 4, 2]]            #src
+        Q = g[1].Q[I, I]                       #src
+        @test Q[1, 1] ≈ 25/64 rtol=1e-3 #src
+        @test Q[1, 2] ≈ -5/8  rtol=1e-3 #src
+        @test Q[1, 3] ≈  5/8  rtol=1e-3 #src
+        @test Q[2, 2] ≈  1    rtol=1e-3 #src
+        @test Q[2, 3] ≈ -1    rtol=1e-3 #src
+        @test Q[3, 3] ≈  1    rtol=1e-3 #src
+    end #src
+    @test g[2].basis.polynomials == [x^2 + y^2, 1.0] #src
+    @test g[2].Q[2, 2] ≈ 7921/4096 rtol=1e-3 #src
+    @test g[2].Q[1, 2] ≈  -89/128 rtol=1e-3  #src
+    @test g[2].Q[1, 1] ≈ 1/4 rtol=1e-3 #src
+    @test g[3].basis.polynomials == [x * y] #src
+    @test g[3].Q[1, 1] ≈ 0   atol=1e-3 #src
+    @test g[4].basis.polynomials == [x^2 - y^2] #src
+    @test g[4].Q[1, 1] ≈ 0   atol=1e-3 #src
+    for g in gram_matrix(con_ref).sub_gram_matrices
+        println(g.basis.polynomials)
+    end
 end
+solve(G)
+
+# We notice that we indeed find `-3825/4096` and that symmetry was exploited.
+# In case the conjugacy classes are known, we can implement
+# `SymbolicWedderburn.conjugacy_classes_orbit` instead of `gens`, `order` and `iterate`.
+# To show that these do not need to be implemented, we create a new dihedral group type
+# that do not implement these methods but that instead implement
+# `SymbolicWedderburn.conjugacy_classes_orbit`:
+
+struct DihedralGroup2 <: Group
+    n::Int
+end
+_orbit(cc::Vector{<:GroupElem}) = PermutationGroups.Orbit(cc, Dict(a => nothing for a in cc))
+_orbit(el::GroupElem) = _orbit([el])
+function SymbolicWedderburn.conjugacy_classes_orbit(d::DihedralGroup2)
+    orbits = [_orbit(DihedralElement(d.n, false, 0))]
+    for i in 1:div(d.n - 1, 2)
+        push!(orbits, _orbit([
+            DihedralElement(d.n, false, i),
+            DihedralElement(d.n, false, d.n - i),
+        ]))
+    end
+    if iseven(d.n)
+        push!(orbits, _orbit(DihedralElement(d.n, false, div(d.n, 2))))
+        push!(orbits, _orbit([
+            DihedralElement(d.n, true, i) for i in 0:2:(d.n - 2)
+        ]))
+        push!(orbits, _orbit([
+            DihedralElement(d.n, true, i) for i in 1:2:(d.n - 1)
+        ]))
+    else
+        push!(orbits, _orbit([
+            DihedralElement(d.n, true, i) for i in 0:(d.n - 1)
+        ]))
+    end
+end
+
+# As we have not implemented the iterator over all the elements, we can iterate over all
+# conjugacy classes instead to verify that the polynomial is invariant under the group action.
+
+G = DihedralGroup2(4)
+for cc in SymbolicWedderburn.conjugacy_classes_orbit(G)
+    for g in cc
+        @show action(poly, g)
+        @test action(poly, g) == poly #src
+    end
+end
+
+solve(G)
