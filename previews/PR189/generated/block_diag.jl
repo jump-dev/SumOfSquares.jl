5e37b48a430181cb09f674d41b0cbba79d82e47e
Documenter.jl <documenter@juliadocs.github.io>
2021-04-08 12:34:00 +0000

    build based on 1a15e33

--- previews/PR189/generated/block_diag.jl
+++ previews/PR189/generated/block_diag.jl
@@ -1,5 +1,5 @@
 using LinearAlgebra
-using DataStructures
+using Combinatorics, DataStructures
 
 function row_echelon_linsolve(A::Matrix{T}, b::Vector{T}) where {T}
     j = 0
@@ -12,41 +12,81 @@ function row_echelon_linsolve(A::Matrix{T}, b::Vector{T}) where {T}
         end
         error("Not in row_echelon_form, cannot find for `$i`th entry.")
     end
-    @assert x'A == b'
+    @assert transpose(A) * x == b
     return x
 end
 
-function block_diag(As, m)
+function ordered_block_diag(As, d)
+    U = block_diag(As, d)
+    U === nothing && return nothing
+    iU = U'
+    @assert iU ≈ inv(U)
+    Bs = [iU * A * U for A in As]
+    @assert all(Bs) do B
+        isblockdim(B, d)
+    end
+    refs = [B[1:d, 1:d] for B in Bs]
+    offset = d
+    for offset in d:d:(size(U, 1) - d)
+        I = offset .+ (1:d)
+        Cs = [B[I, I] for B in Bs]
+        σ_ok = nothing
+        for σ in permutations(1:d)
+            if all(zip(refs, Cs)) do refC
+                ref, C = refC
+                isapprox(ref[σ, σ], C, rtol=1e-8)
+            end
+                σ_ok = σ
+                break
+            end
+        end
+        if σ_ok === nothing
+            error("No permutation can make $refs and $Cs match")
+        end
+        U[:, I] = U[:, I[σ_ok]]
+        offset += d
+    end
+    ordered_block_check(U, As, d)
+    return U
+end
+
+function ordered_block_check(U, As, d)
+    iU = U'
+    @assert iU ≈ inv(U)
+    @assert all(As) do A
+        is_ordered_blockdim(iU * A * U, d)
+    end
+end
+
+function block_diag(As, d)
     for A in As
         #T = eigen(A).vectors
         T = schur(A).vectors
-        #@show D
-        iT = inv(T)
+        iT = T'
+        @assert iT ≈ inv(T)
         n = LinearAlgebra.checksquare(A)
         union_find = IntDisjointSets(n)
         Bs = [iT * A * T for A in As]
         for B in Bs
             merge_sparsity!(union_find, B)
-            #@show union_find
         end
-        d = Dict{Int,Vector{Int}}()
+        blocks = Dict{Int,Vector{Int}}()
         for i in 1:n
             r = find_root!(union_find, i)
-            d[r] = push!(get(d, r, Int[]), i)
+            blocks[r] = push!(get(blocks, r, Int[]), i)
         end
-        #@show d
-        if length(d) > 1
+        if length(blocks) > 1
             U = similar(T)
             offset = 0
-            for v in values(d)
-                @assert iszero(length(v) % m)
-                if length(v) == m
+            for v in values(blocks)
+                @assert iszero(length(v) % d)
+                if length(v) == d
                     V = T[:, v]
                 else
                     Cs = [B[v, v] for B in Bs]
-                    V = block_diag(Cs, m)
+                    V = block_diag(Cs, d)
                     V === nothing && break
-                    V *= T[:, v]'
+                    V *= transpose(T[:, v])
                 end
                 U[:, offset .+ eachindex(v)] = V
                 offset += length(v)
@@ -67,10 +107,18 @@ function merge_sparsity!(union_find::IntDisjointSets, A, tol=1e-8)
     end
 end
 
-function isblockdim(A, m, tol=1e-8)
+function is_ordered_blockdim(A, d, tol=1e-8)
+    B = A[1:d, 1:d]
+    return isblockdim(A, d, tol) &&
+        all(d:d:(size(A, 1) - d)) do offset
+            I = offset .+ (1:d)
+            isapprox(B, A[I, I], rtol=tol)
+        end
+end
+function isblockdim(A, d, tol=1e-8)
     for I in CartesianIndices(A)
         i, j = I.I
-        if abs(i - j) >= m && abs(A[I]) > tol
+        if abs(i - j) >= d && abs(A[I]) > tol
             return false
         end
     end
