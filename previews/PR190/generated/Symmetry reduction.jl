f020a9a267a8bc10130ec2d8b25113ec070a642e
Documenter.jl <documenter@juliadocs.github.io>
2021-04-08 13:24:12 +0000

    build based on 3e4b75d

---
+++ previews/PR190/generated/Symmetry reduction.jl
@@ -0,0 +1,45 @@
+import MutableArithmetics
+const MA = MutableArithmetics
+using MultivariatePolynomials
+const MP = MultivariatePolynomials
+using MultivariateBases
+const MB = MultivariateBases
+
+using DynamicPolynomials
+@polyvar x[1:4]
+
+poly = sum(x) + sum(x.^2)
+
+using SumOfSquares
+include(joinpath(dirname(dirname(pathof(SumOfSquares))), "examples", "symmetry.jl"))
+
+using PermutationGroups
+function action(mono::AbstractMonomial, p::Perm)
+    v = variables(mono)
+    MP.substitute(MP.Eval(), mono, v => [v[i^p] for i in eachindex(v)])
+end
+function action(term::MP.AbstractTerm, el::Perm)
+    return MP.coefficient(term) * action(MP.monomial(term), el)
+end
+function action(poly::MP.AbstractPolynomial, el::Perm)
+    return MP.polynomial([action(term, el) for term in MP.terms(poly)])
+end
+
+G = PermGroup([perm"(1,2,3,4)"])
+
+import CSDP
+solver = CSDP.Optimizer
+model = Model(solver)
+@variable(model, t)
+@objective(model, Max, t)
+certificate = SymmetricIdeal(Certificate.MaxDegree(SOSCone(), MonomialBasis, maxdegree(poly)), G, action)
+con_ref = @constraint(model, poly - t in SOSCone(), ideal_certificate = certificate)
+optimize!(model)
+value(t)
+
+for g in gram_matrix(con_ref).sub_gram_matrices
+    println(g.basis.polynomials)
+end
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
