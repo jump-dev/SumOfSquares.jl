9400fedc7fe724dbe61207c96602934c8c9c1df5
odow <o.dowson@gmail.com>
2021-04-22 11:46:42 +1200

    Fix printing

--- test/constraint.jl
+++ test/constraint.jl
@@ -15,19 +15,19 @@ end
     @variable(model, a)
     cref = @constraint(model, a * x^2 >= 1)
     @test sprint(show, MIME"text/plain"(), cref) == "(a)x² + (-1) is SOS"
-    @test sprint(show, MIME"text/latex"(), cref) == "\$ (a)x^{2} + (-1) \\text{ is SOS} \$"
+    @test sprint(show, MIME"text/latex"(), cref) == "\$\$ (a)x^{2} + (-1) \\text{ is SOS} \$\$"
     sdref = @constraint(model, a * x^2 in SDSOSCone())
     @test sprint(show, MIME"text/plain"(), sdref) == "(a)x² is SDSOS"
-    @test sprint(show, MIME"text/latex"(), sdref) == "\$ (a)x^{2} \\text{ is SDSOS} \$"
+    @test sprint(show, MIME"text/latex"(), sdref) == "\$\$ (a)x^{2} \\text{ is SDSOS} \$\$"
     dref = @constraint(model, a * x^2 in DSOSCone())
     @test sprint(show, MIME"text/plain"(), dref) == "(a)x² is DSOS"
-    @test sprint(show, MIME"text/latex"(), dref) == "\$ (a)x^{2} \\text{ is DSOS} \$"
+    @test sprint(show, MIME"text/latex"(), dref) == "\$\$ (a)x^{2} \\text{ is DSOS} \$\$"
     for sparsity in [NoSparsity(), VariableSparsity()]
         cref_fix = @constraint(model, a * x^2 >= 1, domain = (@set x == 1), sparsity = sparsity)
         @test sprint(show, MIME"text/plain"(), cref_fix) == "(a)x² + (-1) is SOS"
-        @test sprint(show, MIME"text/latex"(), cref_fix) == "\$ (a)x^{2} + (-1) \\text{ is SOS} \$"
+        @test sprint(show, MIME"text/latex"(), cref_fix) == "\$\$ (a)x^{2} + (-1) \\text{ is SOS} \$\$"
         cref_fix = @constraint(model, a * x^2 >= 1, domain = (@set x >= 1), sparsity = sparsity)
         @test sprint(show, MIME"text/plain"(), cref_fix) == "(a)x² + (-1) is SOS"
-        @test sprint(show, MIME"text/latex"(), cref_fix) == "\$ (a)x^{2} + (-1) \\text{ is SOS} \$"
+        @test sprint(show, MIME"text/latex"(), cref_fix) == "\$\$ (a)x^{2} + (-1) \\text{ is SOS} \$\$"
     end
 end
