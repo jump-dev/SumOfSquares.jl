bd4c32aed60db736f44e63b86d3c1497ef4725da
Documenter.jl <documenter@juliadocs.github.io>
2021-09-12 13:46:38 +0000

    build based on 467f1c7

---
+++ previews/PR212/generated/Noncommutative and Hermitian/noncommutative_variables.jl
@@ -0,0 +1,39 @@
+using DynamicPolynomials
+@ncpolyvar x y
+p = (x * y + x^2)^2
+
+using SumOfSquares
+import CSDP
+optimizer_constructor = optimizer_with_attributes(CSDP.Optimizer, MOI.Silent() => true)
+model = Model(optimizer_constructor)
+con_ref = @constraint(model, p in SOSCone())
+
+optimize!(model)
+
+certificate_basis(con_ref)
+
+gram_matrix(con_ref).Q
+
+sos_decomposition(con_ref)
+
+sos_decomposition(con_ref, 1e-6)
+
+using DynamicPolynomials
+@ncpolyvar x y
+n = 10
+p = (x + x^n * y^(2n) * x^n)^2
+
+using SumOfSquares
+model = Model(optimizer_constructor)
+con_ref = @constraint(model, p in SOSCone())
+
+optimize!(model)
+
+certificate_basis(con_ref)
+
+gram_matrix(con_ref).Q
+
+sos_decomposition(con_ref, 1e-6)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
---
+++ previews/PR212/tutorials/Symmetry/even_reduction.jl
@@ -0,0 +1,50 @@
+# # Even reduction
+
+#md # [![](https://mybinder.org/badge_logo.svg)](@__BINDER_ROOT_URL__/generated/Symmetry/even_reduction.ipynb)
+#md # [![](https://img.shields.io/badge/show-nbviewer-579ACA.svg)](@__NBVIEWER_ROOT_URL__/generated/Symmetry/even_reduction.ipynb)
+
+using Test #src
+using DynamicPolynomials
+@polyvar x
+
+# We would like to find the minimum value of the polynomial
+
+poly = x^4 - 2x^2
+
+using SumOfSquares
+
+# We define the custom action as follows:
+
+struct OnSign <: Symmetry.OnMonomials end
+using PermutationGroups
+import SymbolicWedderburn
+SymbolicWedderburn.coeff_type(::OnSign) = Float64
+function SymbolicWedderburn.action(::OnSign, p::Permutation, mono::AbstractMonomial)
+    if isone(p) || iseven(DynamicPolynomials.degree(mono))
+        return 1 * mono
+    else
+        @assert p.perm == perm"(1,2)"
+        return -1 * mono
+    end
+end
+G = PermGroup([perm"(1,2)"])
+
+# We can exploit the symmetry as follows:
+
+import CSDP
+solver = CSDP.Optimizer
+model = Model(solver)
+@variable(model, t)
+@objective(model, Max, t)
+pattern = Symmetry.Pattern(G, OnSign())
+con_ref = @constraint(model, poly - t in SOSCone(), symmetry = pattern)
+optimize!(model)
+@test value(t) ≈ -1 #src
+value(t)
+
+# We indeed find `-1`, let's verify that symmetry was exploited:
+
+@test length(gram_matrix(con_ref).sub_gram_matrices) == 2 #src
+@test gram_matrix(con_ref).sub_gram_matrices[1].basis.polynomials == [x^2, 1] #src
+@test gram_matrix(con_ref).sub_gram_matrices[2].basis.polynomials == [x] #src
+gram_matrix(con_ref)
