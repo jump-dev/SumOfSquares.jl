9d16a53580e24188b77b7fc6a8f9333550146f77
Benoît Legat <benoit.legat@gmail.com>
2021-11-24 15:17:47 -0500

    Remove method rendered useless by MOI v0.10

--- src/sos_polynomial.jl
+++ src/sos_polynomial.jl
@@ -14,14 +14,6 @@ function vectorized_matrix(Q, basis, ::Type{COI.HermitianPositiveSemidefiniteCon
 end
 
 # Need these two methods to avoid ambiguity
-function build_gram_matrix(q::Vector{MOI.VariableIndex},
-                           basis::AbstractPolynomialBasis, matrix_cone_type, T::Type)
-    return build_gram_matrix(q, basis, matrix_cone_type, T)
-end
-#function build_gram_matrix(q::Vector{MOI.VariableIndex},
-#                           basis::AbstractPolynomialBasis, T::Type, vectorization::HermitianVectorized)
-#    return build_gram_matrix(q, basis, T, vectorization)
-#end
 function build_gram_matrix(q::Vector,
                            basis::AbstractPolynomialBasis, matrix_cone_type, T::Type)
     n = length(basis)
