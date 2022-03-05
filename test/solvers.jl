30b9e415cb38bd7939e36e73f95769738cb22326
Benoît Legat <benoit.legat@gmail.com>
2022-03-05 13:09:56 -0500

    Update solver tol

--- test/solvers.jl
+++ test/solvers.jl
@@ -19,11 +19,11 @@ sdp_factories = MOI.OptimizerWithAttributes[]
 # https://github.com/JuliaOpt/Mosek.jl/issues/92
 # and at least Mosek 8.0.0.41 for sosdemo6 to pass; see:
 # https://github.com/JuliaOpt/Mosek.jl/issues/98
-mos && push!(sdp_factories, optimizer_with_attributes(MosekTools.Mosek.Optimizer, "QUIET" => true))
+mos && push!(sdp_factories, optimizer_with_attributes(MosekTools.Mosek.Optimizer, MOI.Silent() => true))
 # Currently, need to create a file param.csdp to avoid printing, see https://github.com/JuliaOpt/CSDP.jl/issues/15
-csd && push!(sdp_factories, optimizer_with_attributes(CSDP.Optimizer, "printlevel" => 0))
+csd && push!(sdp_factories, optimizer_with_attributes(CSDP.Optimizer, MOI.Silent() => true))
 iscsdp(factory) = occursin("CSDP", string(factory.optimizer_constructor))
 # Need 54000 iterations for sosdemo3 to pass on Linux 64 bits
 # With 55000, sosdemo3 passes for every platform except Windows 64 bits on AppVeyor
-scs && push!(sdp_factories, optimizer_with_attributes(SCS.Optimizer, "eps" => 1e-6, "max_iters" => 60000, "verbose" => 0))
+scs && push!(sdp_factories, optimizer_with_attributes(SCS.Optimizer, "eps_abs" => 1e-6, "max_iters" => 60000, MOI.Silent() => true))
 isscs(factory) = occursin("SCS", string(factory.optimizer_constructor))
