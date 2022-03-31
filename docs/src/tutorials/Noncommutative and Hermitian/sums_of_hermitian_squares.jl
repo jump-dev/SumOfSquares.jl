af2432654cbe33bf7820843dfaa247856735f166
Benoît Legat <benoit.legat@gmail.com>
2022-03-31 08:44:52 -0400

    Simplify hermitian examples

--- docs/src/tutorials/Noncommutative and Hermitian/sums_of_hermitian_squares.jl
+++ docs/src/tutorials/Noncommutative and Hermitian/sums_of_hermitian_squares.jl
@@ -14,17 +14,14 @@ p = (x + 1.0im * y) * (x - im * y)
 
 import CSDP
 model = Model(CSDP.Optimizer)
-add_bridge(model, SumOfSquares.Bridges.Constraint.EmptyBridge)
-add_bridge(model, SumOfSquares.Bridges.Constraint.PositiveSemidefinite2x2Bridge)
 add_bridge(model, SumOfSquares.COI.Bridges.Variable.HermitianToSymmetricPSDBridge)
 add_bridge(model, SumOfSquares.COI.Bridges.Constraint.SplitZeroBridge)
 MOI.Bridges.add_bridge(backend(model).optimizer, PolyJuMP.ZeroPolynomialBridge{Complex{Float64}})
 MOI.Bridges.add_bridge(backend(model).optimizer, SumOfSquares.Bridges.Constraint.SOSPolynomialBridge{Complex{Float64}})
 cone = NonnegPolyInnerCone{SumOfSquares.COI.HermitianPositiveSemidefiniteConeTriangle}()
-certificate = SumOfSquares.Certificate.MaxDegree(cone, MonomialBasis, 2)
-c = SumOfSquares.add_constraint(model, p, cone, ideal_certificate = certificate)
+con_ref = @constraint(model, p in cone)
 optimize!(model)
-dec = sos_decomposition(c, 1e-6) #src
+dec = sos_decomposition(con_ref, 1e-6) #src
 @test length(dec.ps) == 1 #src
 @test sign(real(first(coefficients(dec.ps[1])))) * dec.ps[1] ≈ x - im * y atol=1e-6 rtol=1e-6 #src
-sos_decomposition(c, 1e-6)
+sos_decomposition(con_ref, 1e-6)
