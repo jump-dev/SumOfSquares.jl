4710fcb7899a7fcec89e2392652df6692a632208
Benoît Legat <benoit.legat@gmail.com>
2023-03-23 15:23:59 +0100

    Reenable CSDP tests
    They are fixed by https://github.com/jump-dev/CSDP.jl/pull/89

--- test/Solvers/csdp_tests.jl
+++ test/Solvers/csdp_tests.jl
@@ -6,18 +6,5 @@ config = MOI.Test.Config(atol = 1e-4, rtol = 1e-4)
     Tests.linear_test(factory, config)
 end
 @testset "SDP" begin
-    Tests.sd_test(
-        factory,
-        config,
-        [
-            "sosdemo5_infeasible",
-            "maxcut",
-            "quadratic_infeasible_lyapunov_switched_system",
-            "quadratic_infeasible_scaled_lyapunov_switched_system",
-            "quartic_infeasible_lyapunov_switched_system",
-            "motzkin",
-            "quartic_infeasible_scaled_lyapunov_switched_system",
-            "choi",
-        ],
-    )
+    Tests.sd_test(factory, config)
 end
