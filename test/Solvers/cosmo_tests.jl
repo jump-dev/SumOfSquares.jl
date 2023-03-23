5d12b9da2ba17a1eb45879ed84aaaac9c7fff371
Benoît Legat <benoit.legat@gmail.com>
2023-03-23 14:41:13 +0100

    Fix format

--- test/Solvers/cosmo_tests.jl
+++ test/Solvers/cosmo_tests.jl
@@ -1,7 +1,11 @@
 include("solver_preamble.jl")
 using COSMO
 # sosdemo9: 33000 < max_iter < 34000
-factory = optimizer_with_attributes(COSMO.Optimizer, "verbose" => false, "max_iter" => 34000)
+factory = optimizer_with_attributes(
+    COSMO.Optimizer,
+    "verbose" => false,
+    "max_iter" => 34000,
+)
 config = MOI.Test.Config(atol = 1e-3, rtol = 1e-3)
 @testset "Linear" begin
     Tests.linear_test(factory, config, [
