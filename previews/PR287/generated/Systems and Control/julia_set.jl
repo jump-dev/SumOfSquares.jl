bacdc7741c383bbeaf4505b3a03ec96dbdf67e3f
Documenter.jl <documenter@juliadocs.github.io>
2023-03-27 17:00:14 +0000

    build based on 21b82b8

---
+++ previews/PR287/generated/Systems and Control/julia_set.jl
@@ -0,0 +1,105 @@
+function julia_map(point, c)
+    a, b = point
+    return [a^2 - b^2 + real(c), 2a * b + imag(c)]
+end
+
+escape_radius(c) = (1 + √(1 + 4 * abs(c))) / 2
+
+using LinearAlgebra
+function in_set(x, c, m=2000)
+    r = escape_radius(c)
+    for i in 1:m
+        if norm(x) > r
+            return false
+        end
+        x = julia_map(x, c)
+    end
+    return true
+end
+
+using SpecialFunctions
+using DynamicPolynomials
+β(α) = (α + 1) / 2
+function circle_integral(mono::AbstractMonomial)
+    if any(isodd, exponents(mono))
+        return 0.0
+    else
+        return 2 * prod(gamma ∘ β, exponents(mono)) / gamma(sum(β, exponents(mono)))
+    end
+end
+function disk_integral(mono::AbstractMonomial, r)
+    d = degree(mono) + nvariables(mono)
+    return circle_integral(mono) * r^d / d
+end
+function disk_integral(p::AbstractPolynomialLike, r)
+    return sum(MultivariatePolynomials.coefficient(t) * disk_integral(monomial(t), r) for t in terms(p))
+end
+
+using SumOfSquares
+function outer_approximation(solver, d::Int, c; α = 1/2)
+    @polyvar x[1:2]
+    model = SOSModel(solver)
+    r = escape_radius(c)
+    S = @set sum(x.^2) <= r^2
+    @variable(model, v, Poly(monomials(x, 0:2d)))
+    @variable(model, w0, SOSPoly(monomials(x, 0:d)))
+    @variable(model, w1, SOSPoly(monomials(x, 0:(d - 1))))
+    @constraint(model, α * v(x => julia_map(x, c)) <= v, domain = S)
+    w = w0 + w1 * (r^2 - sum(x.^2))
+    @constraint(model, w >= v + 1, domain = S)
+    @objective(model, Min, disk_integral(w, r))
+    optimize!(model)
+    if primal_status(model) == MOI.NO_SOLUTION
+        return
+    end
+    return model
+end
+
+using ImplicitPlots
+using Plots
+function julia_plot(poly, c, n=200, m=1000; tol=1e-6, res = 1000)
+    r = escape_radius(c)
+    p = implicit_plot(poly; xlims=(-r, r) .* 1.1, ylims=(-r, r), resolution = res, label="")
+    θ = range(0, stop=2π, length=100)
+    points = Vector{Float64}[]
+    as = range(-r, r, length=n)
+    bs = range(-r, r, length=n)
+    for a in as, b in bs
+        point = [a, b]
+        if in_set(point, c, m)
+            push!(points, point)
+        end
+    end
+    xs = [point[1] for point in points]
+    ys = [point[2] for point in points]
+    scatter!(p, xs, ys, label="", markerstrokewidth=0, markersize=1.5, m=:pixel)
+    return p
+end
+
+import CSDP
+solver = optimizer_with_attributes(CSDP.Optimizer, MOI.Silent() => true)
+
+c = -0.7 + 0.2im
+model = outer_approximation(solver, 2, c)
+solution_summary(model)
+
+julia_plot(value(model[:v]), c)
+
+model = outer_approximation(solver, 4, c)
+solution_summary(model)
+
+julia_plot(value(model[:v]), c)
+
+c = -0.9 + 0.2im
+model = outer_approximation(solver, 2, c)
+solution_summary(model)
+
+julia_plot(value(model[:v]), c)
+
+model = outer_approximation(solver, 4, c)
+solution_summary(model)
+
+julia_plot(value(model[:v]), c)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
---
+++ previews/PR287/tutorials/Extension/certificate.jl
@@ -0,0 +1,112 @@
+# # Certificate
+
+#md # [![](https://mybinder.org/badge_logo.svg)](@__BINDER_ROOT_URL__/generated/Extension/certificate.ipynb)
+#md # [![](https://img.shields.io/badge/show-nbviewer-579ACA.svg)](@__NBVIEWER_ROOT_URL__/generated/Extension/certificate.ipynb)
+# **Contributed by**: Benoît Legat
+
+# ## Introduction
+
+# Consider the polynomial optimization problem (a variation from [L09, Example 2.2]) of
+# minimizing the polynomial $x^3 - x^2 + 2xy - y^2 + y^3$
+# over the polyhedron defined by the inequalities $x \ge 0, y \ge 0$ and $x + y \geq 1$.
+
+# [L09] Lasserre, J. B.
+# *Moments, positive polynomials and their applications*.
+# World Scientific, **2009**.
+
+using Test #src
+using DynamicPolynomials
+@polyvar x y
+p = x^3 - x^2 + 2x*y - y^2 + y^3 + x^3 * y
+using SumOfSquares
+S = @set x >= 0 && y >= 0 && x^2 + y^2 >= 2
+
+# We will now see how to find the optimal solution using Sum of Squares Programming.
+# We first need to pick an SDP solver, see [here](https://jump.dev/JuMP.jl/v1.8/installation/#Supported-solvers) for a list of the available choices.
+# Note that SumOfSquares generates a *standard form* SDP (i.e., SDP variables
+# and equality constraints) while SCS expects a *geometric form* SDP (i.e.,
+# free variables and symmetric matrices depending affinely on these variables
+# constrained to belong to the PSD cone).
+# JuMP will transform the standard from to the geometric form will create the PSD
+# variables as free variables and then constrain then to be PSD.
+# While this will work, since the dual of a standard from is in in geometric form,
+# dualizing the problem will generate a smaller SDP.
+# We use therefore `Dualization.dual_optimizer` so that SCS solves the dual problem.
+
+import SCS
+using Dualization
+solver = dual_optimizer(SCS.Optimizer)
+
+# A Sum-of-Squares certificate that $p \ge \alpha$ over the domain `S`, ensures that $\alpha$ is a lower bound to the polynomial optimization problem.
+# The following program searches for the largest lower bound.
+
+model = SOSModel(solver)
+@variable(model, α)
+@objective(model, Max, α)
+@constraint(model, c, p >= α, domain = S)
+optimize!(model)
+
+# We can see that the problem is infeasible, meaning that no lower bound was found.
+
+@test termination_status(model) == MOI.INFEASIBLE #src
+@test dual_status(model) == MOI.INFEASIBILITY_CERTIFICATE #src
+solution_summary(model)
+
+# We now define the Schmüdgen's certificate:
+
+import MultivariateBases as MB
+const SOS = SumOfSquares
+const SOSC = SOS.Certificate
+struct Schmüdgen{IC <: SOSC.AbstractIdealCertificate, CT <: SOS.SOSLikeCone, BT <: SOS.AbstractPolynomialBasis} <: SOSC.AbstractPreorderCertificate
+    ideal_certificate::IC
+    cone::CT
+    basis::Type{BT}
+    maxdegree::Int
+end
+
+SOSC.cone(certificate::Schmüdgen) = certificate.cone
+
+function SOSC.preprocessed_domain(::Schmüdgen, domain::BasicSemialgebraicSet, p)
+    return SOSC.with_variables(domain, p)
+end
+
+function SOSC.preorder_indices(::Schmüdgen, domain::SOSC.WithVariables)
+    n = length(domain.inner.p)
+    if n >= Sys.WORD_SIZE
+        error("There are $(2^n - 1) products in Schmüdgen's certificate, they cannot even be indexed with `$Int`.")
+    end
+    return map(SOSC.PreorderIndex, 1:(2^n-1))
+end
+
+function SOSC.multiplier_basis(certificate::Schmüdgen, index::SOSC.PreorderIndex, domain::SOSC.WithVariables)
+    q = SOSC.generator(certificate, index, domain)
+    return SOSC.maxdegree_gram_basis(certificate.basis, variables(domain), SOSC.multiplier_maxdegree(certificate.maxdegree, q))
+end
+function SOSC.multiplier_basis_type(::Type{Schmüdgen{IC, CT, BT}}) where {IC, CT, BT}
+    return BT
+end
+
+function SOSC.generator(::Schmüdgen, index::SOSC.PreorderIndex, domain::SOSC.WithVariables)
+    I = [i for i in eachindex(domain.inner.p) if !iszero(index.value & (1 << (i - 1)))]
+    return prod([domain.inner.p[i] for i in eachindex(domain.inner.p) if !iszero(index.value & (1 << (i - 1)))])
+end
+
+SOSC.ideal_certificate(certificate::Schmüdgen) = certificate.ideal_certificate
+SOSC.ideal_certificate(::Type{<:Schmüdgen{IC}}) where {IC} = IC
+
+SOS.matrix_cone_type(::Type{<:Schmüdgen{IC, CT}}) where {IC, CT} = SOS.matrix_cone_type(CT)
+
+# Let's try again with our the Schmüdgen certificate:
+
+model = SOSModel(solver)
+@variable(model, α)
+@objective(model, Max, α)
+ideal_certificate = SOSC.Newton(SOSCone(), MB.MonomialBasis, tuple())
+certificate = Schmüdgen(ideal_certificate, SOSCone(), MB.MonomialBasis, maxdegree(p))
+@constraint(model, c, p >= α, domain = S, certificate = certificate)
+optimize!(model)
+@test termination_status(model) == MOI.OPTIMAL #src
+@test primal_status(model) == MOI.FEASIBLE_POINT #src
+@test objective_value(model) ≈ 0.8284 rtol=1e-3 #src
+@test length(lagrangian_multipliers(c)) == 7 #src
+solution_summary(model)
