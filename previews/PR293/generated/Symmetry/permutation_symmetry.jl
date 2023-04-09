9b7ed743b82129eab9ab54c2ffb34a6f37f4bb5b
Documenter.jl <documenter@juliadocs.github.io>
2023-04-09 22:10:28 +0000

    build based on 6e39575

---
+++ previews/PR293/generated/Symmetry/permutation_symmetry.jl
@@ -0,0 +1,30 @@
+import MutableArithmetics as MA
+using MultivariatePolynomials
+using MultivariateBases
+
+using DynamicPolynomials
+@polyvar x[1:4]
+
+poly = sum(x) + sum(x.^2)
+
+using SumOfSquares
+
+using PermutationGroups
+G = PermGroup([perm"(1,2,3,4)"])
+
+import CSDP
+solver = CSDP.Optimizer
+model = Model(solver)
+@variable(model, t)
+@objective(model, Max, t)
+pattern = Symmetry.Pattern(G, Symmetry.VariablePermutation())
+con_ref = @constraint(model, poly - t in SOSCone(), symmetry = pattern)
+optimize!(model)
+value(t)
+
+for g in gram_matrix(con_ref).sub_gram_matrices
+    println(g.basis.polynomials)
+end
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
