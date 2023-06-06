e2f10261bb7702f06e7df9d0d025d75ce2d80bd0
Benoît Legat <benoit.legat@gmail.com>
2023-06-06 13:18:55 +0200

    Fix format

--- src/sosdec.jl
+++ src/sosdec.jl
@@ -54,7 +54,10 @@ function SOSDecomposition(
     # it would be nice to have though
     ldlt =
         MultivariateMoments.low_rank_ldlt(Matrix(value_matrix(p)), dec, ranktol)
-    ps = [MP.polynomial(√ldlt.singular_values[i] * ldlt.L[:, i], p.basis) for i in axes(ldlt.L, 2)]
+    ps = [
+        MP.polynomial(√ldlt.singular_values[i] * ldlt.L[:, i], p.basis) for
+        i in axes(ldlt.L, 2)
+    ]
     return SOSDecomposition(ps)
 end
 # Without LDL^T, we need to do float(T)
