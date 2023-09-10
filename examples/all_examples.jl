bdc7bdd738372ff3658a2c2ddf248fdae092431d
Benoît Legat <benoit.legat@gmail.com>
2023-09-10 21:31:55 +0200

    Convert Goldstein example as tutorial (#315)
    * Move goldstein to tuto
    
    * Convert Goldstein as tutorial
    
    * Use SCS
    
    * Use Clarabel
    
    * Minor improvements
    
    * Fix

--- examples/all_examples.jl
+++ examples/all_examples.jl
@@ -1,2 +1,2 @@
-const EXAMPLES = filter(ex -> endswith(ex, ".jl") && ex != "run_examples.jl" && ex != "all_examples.jl" && ex != "goldsteinprice.jl" && ex != "sparse_polynomials.jl" && ex != "scaled_perm.jl" && ex != "symmetry.jl" && ex != "discourse_65377.jl" && ex != "discourse_65377_1.jl" && ex != "prajna.jl",
+const EXAMPLES = filter(ex -> endswith(ex, ".jl") && ex != "run_examples.jl" && ex != "all_examples.jl" && ex != "sparse_polynomials.jl" && ex != "scaled_perm.jl" && ex != "symmetry.jl" && ex != "discourse_65377.jl" && ex != "discourse_65377_1.jl" && ex != "prajna.jl",
                         readdir(@__DIR__))
