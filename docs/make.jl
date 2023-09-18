9500c3c42b02659edf72b19720d0193518aee3f7
Benoît Legat <benoit.legat@gmail.com>
2023-09-18 07:11:45 +0200

    Update to Documenter v1 (#319)
    * Update to Documenter v1
    
    * Remove strict

--- docs/make.jl
+++ docs/make.jl
@@ -35,8 +35,6 @@ makedocs(
     sitename = "SumOfSquares",
     # See https://github.com/JuliaDocs/Documenter.jl/issues/868
     format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
-    # See https://github.com/jump-dev/JuMP.jl/issues/1576
-    strict = true,
     pages = [
         "Index" => "index.md",
         "Sum-of-Squares Programming" => "sumofsquares.md",
