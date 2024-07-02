6ce945ad23671ebd2b7cc42b87f9e4b1af7e21f5
Benoît Legat <benoit.legat@gmail.com>
2024-07-02 11:24:52 +0200

    Fixes

--- test/runtests.jl
+++ test/runtests.jl
@@ -34,7 +34,7 @@ include("variable.jl")
 include("constraint.jl")
 include("Bridges/Bridges.jl")
 
-#include("Mock/mock_tests.jl")
+include("Mock/mock_tests.jl")
 
 # Tests needing a solver
 # FIXME these tests should be converted to Literate and moved to `examples` or
