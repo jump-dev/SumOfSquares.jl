d8f63502639c0ae64a3f6a03d893f924a6ec3bf0
Benoît Legat <benoit.legat@gmail.com>
2024-07-08 10:32:06 +0200

    Fix format

--- test/Mock/quadratic.jl
+++ test/Mock/quadratic.jl
@@ -1,4 +1,4 @@
-config = MOI.Test.Config(atol=1e-6)
+config = MOI.Test.Config(atol = 1e-6)
 function optimize!(mock)
     return MOI.Utilities.mock_optimize!(
         mock,
@@ -35,10 +35,8 @@ function optimize!(mock)
     return MOI.Utilities.mock_optimize!(
         mock,
         [2.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0],
-        (
-            MOI.VectorOfVariables,
-            MOI.PositiveSemidefiniteConeTriangle,
-        ) => [[0.0, 0.0, 1.0, 0.0, -1.0, 1.0]],
+        (MOI.VectorOfVariables, MOI.PositiveSemidefiniteConeTriangle) =>
+            [[0.0, 0.0, 1.0, 0.0, -1.0, 1.0]],
         (MOI.VectorAffineFunction{Float64}, MOI.Zeros) =>
             [[0.0, 0.0, 0.0, 2.0, -1.0, 2.0]],
     )
@@ -75,20 +73,43 @@ for mock in mocks(optimize!)
     Tests.dsos_cheby_univariate_quadratic_test(mock, config)
 end
 function optimize!(mock)
-    return MOI.Utilities.mock_optimize!(mock,
-(MOI.FEASIBLE_POINT, [2.0, 1.0, 1.0, 1.0, 1.0]),
-(MOI.ScalarAffineFunction{Float64}, MOI.GreaterThan{Float64}) => [0.0, 2.0, 1.0, 1.0],
-(MOI.VectorAffineFunction{Float64}, MOI.Zeros) => [[1.0, -√2, 1.0]],
-)
+    return MOI.Utilities.mock_optimize!(
+        mock,
+        (MOI.FEASIBLE_POINT, [2.0, 1.0, 1.0, 1.0, 1.0]),
+        (MOI.ScalarAffineFunction{Float64}, MOI.GreaterThan{Float64}) =>
+            [0.0, 2.0, 1.0, 1.0],
+        (MOI.VectorAffineFunction{Float64}, MOI.Zeros) => [[1.0, -√2, 1.0]],
+    )
 end
 for mock in mocks(optimize!)
     Tests.dsos_scaled_bivariate_quadratic_test(mock, config)
 end
 function optimize!(mock)
-    return MOI.Utilities.mock_optimize!(mock,
-        (MOI.FEASIBLE_POINT, [2.0, 0.0, 0.0, 1.0, -0.0, 1.0, 1.0, 0.0, 0.0, 1.0]),
-        (MOI.ScalarAffineFunction{Float64}, MOI.GreaterThan{Float64}) => [0.7624324421943409, 0.7624324421943427, 0.7624324421943439, 0.7624324421943471, 0.0, 2.0, 0.5248650317582692, 1.0, 1.0],
-        (MOI.VectorAffineFunction{Float64}, MOI.Zeros) => [[0.5248650317582707, 0.0, 0.0, 1.4751346735031994, -1.0, 1.4751346735032143]],
+    return MOI.Utilities.mock_optimize!(
+        mock,
+        (
+            MOI.FEASIBLE_POINT,
+            [2.0, 0.0, 0.0, 1.0, -0.0, 1.0, 1.0, 0.0, 0.0, 1.0],
+        ),
+        (MOI.ScalarAffineFunction{Float64}, MOI.GreaterThan{Float64}) => [
+            0.7624324421943409,
+            0.7624324421943427,
+            0.7624324421943439,
+            0.7624324421943471,
+            0.0,
+            2.0,
+            0.5248650317582692,
+            1.0,
+            1.0,
+        ],
+        (MOI.VectorAffineFunction{Float64}, MOI.Zeros) => [[
+            0.5248650317582707,
+            0.0,
+            0.0,
+            1.4751346735031994,
+            -1.0,
+            1.4751346735032143,
+        ]],
     )
 end
 for mock in mocks(optimize!)
