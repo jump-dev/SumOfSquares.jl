f832bd1afc92e0d0e8841aa9eb2530e9145b6b48
Documenter.jl <documenter@juliadocs.github.io>
2024-07-09 18:36:07 +0000

    build based on 7c5d298

---
+++ previews/PR377/generated/Getting started/sos_decomposition.jl
@@ -0,0 +1,21 @@
+using DynamicPolynomials
+using SumOfSquares
+import CSDP
+
+@polyvar x y
+p = x^2 - x*y^2 + y^4 + 1
+
+model = SOSModel(CSDP.Optimizer)
+@constraint(model, cref, p >= 0)
+
+optimize!(model)
+
+sos_dec = sos_decomposition(cref, 1e-4)
+
+polynomial(sos_dec, Float32)
+
+gram = gram_matrix(cref)
+
+gram.basis.monomials' * gram.Q * gram.basis.monomials
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
