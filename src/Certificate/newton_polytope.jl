aabcab257220f8cee3d99a2e7c600db89c0fef0f
Benoît Legat <benoit.legat@gmail.com>
2025-04-14 14:53:16 +0200

    Refactor Newton Polytope to avoid 3-factor products (#397)
    * Refactor Newton Polytope to avoid 3-factor products
    
    * Switch to SignCount in cache
    
    * Remove unused cache
    
    * Remove debug
    
    * Fix format

--- src/Certificate/newton_polytope.jl
+++ src/Certificate/newton_polytope.jl
@@ -534,12 +534,7 @@ function half_newton_polytope(
 )
     return half_newton_polytope(
         p,
-        [
-            MB.algebra_element(
-                MP.coefficients(g),
-                MB.SubBasis{MB.Monomial}(MP.monomials(g)),
-            ) for g in gs
-        ],
+        MB.algebra_element.(gs),
         vars,
         maxdegree,
         filter,
@@ -592,6 +587,18 @@ function _sign(c::SignCount)
     end
 end
 
+function Base.:*(α, a::SignCount)
+    if α > 0
+        return a
+    elseif α < 0
+        return SignCount(a.unknown, a.negative, a.positive)
+    else
+        error("Cannot multiply `SignCount`` with `$α`")
+    end
+end
+
+Base.:*(a::SignCount, α) = α * a
+
 function Base.:+(a::SignCount, b::SignCount)
     return SignCount(
         a.unknown + b.unknown,
@@ -601,16 +608,16 @@ function Base.:+(a::SignCount, b::SignCount)
 end
 
 function Base.:+(c::SignCount, a::SignChange{Missing})
-    @assert c.unknown >= -a.Δ
+    #@assert c.unknown >= -a.Δ
     return SignCount(c.unknown + a.Δ, c.positive, c.negative)
 end
 
 function Base.:+(c::SignCount, a::SignChange{<:Number})
     if a.sign > 0
-        @assert c.positive >= -a.Δ
+        #@assert c.positive >= -a.Δ
         return SignCount(c.unknown, c.positive + a.Δ, c.negative)
     elseif a.sign < 0
-        @assert c.negative >= -a.Δ
+        #@assert c.negative >= -a.Δ
         return SignCount(c.unknown, c.positive, c.negative + a.Δ)
     elseif iszero(a.sign)
         error(
@@ -652,6 +659,7 @@ end
 
 SA.nonzero_pairs(d::_DictCoefficients) = d.inner
 Base.keys(d::_DictCoefficients) = keys(d.inner)
+Base.haskey(d::_DictCoefficients, k) = haskey(d.inner, k)
 
 Base.getindex(d::_DictCoefficients{K}, key::K) where {K} = d.inner[key]
 
@@ -707,7 +715,10 @@ function post_filter(
         _DictCoefficients(Dict{MP.monomial_type(typeof(poly)),SignCount}()),
         MB.implicit_basis(SA.basis(poly)),
     )
-    cache = zero(Float64, MB.algebra(MB.implicit_basis(SA.basis(poly))))
+    algebra = MB.algebra(MB.implicit_basis(SA.basis(poly)))
+    cache = zero(Float64, algebra)
+    cache3 = zero(SignCount, algebra)
+    cache4 = zero(SignCount, algebra)
     for (mono, v) in SA.nonzero_pairs(SA.coeffs(poly))
         MA.operate!(
             SA.UnsafeAddMul(*),
@@ -726,21 +737,16 @@ function post_filter(
             )
         end
     end
-    function decrease(sign, a, b, c)
+    function decrease(sign, a, b, generator)
         MA.operate_to!(
-            cache,
+            cache3,
             *,
-            MB.algebra_element(a),
+            _term(SignChange(sign, -1), a),
             MB.algebra_element(b),
-            MB.algebra_element(c),
         )
-        MA.operate!(
-            SA.UnsafeAddMul(*),
-            counter,
-            _term_constant_monomial(SignChange(sign, -1), a),
-            cache,
-        )
-        for mono in SA.supp(cache)
+        MA.operate_to!(cache4, *, cache3, generator)
+        MA.operate!(SA.UnsafeAddMul(*), counter, cache4)
+        for mono in SA.supp(cache4)
             count = SA.coeffs(counter)[SA.basis(counter)[mono]]
             count_sign = _sign(count)
             # This means the `counter` has a sign and it didn't have a sign before
@@ -764,36 +770,31 @@ function post_filter(
         end
         keep[i][j] = false
         a = multipliers_gram_monos[i][j]
-        for (k, v) in SA.nonzero_pairs(SA.coeffs(generators[i]))
-            mono = SA.basis(generators[i])[k]
-            sign = -_sign(v)
-            decrease(sign, mono, a, a)
-            for (j, b) in enumerate(multipliers_gram_monos[i])
-                if keep[i][j]
-                    decrease(missing, mono, a, b)
-                    decrease(missing, mono, b, a)
-                end
+        decrease(-1, a, a, generators[i])
+        for (k, b) in enumerate(multipliers_gram_monos[i])
+            if keep[i][k]
+                decrease(missing, a, b, generators[i])
+                decrease(missing, b, a, generators[i])
             end
         end
     end
     for i in eachindex(generators)
-        for k in SA.supp(generators[i])
-            for (j, mono) in enumerate(multipliers_gram_monos[i])
-                MA.operate_to!(
-                    cache,
-                    *,
-                    MB.algebra_element(k),
-                    MB.algebra_element(mono),
-                    MB.algebra_element(mono),
-                )
-                for w in SA.supp(cache)
-                    if ismissing(
-                        _sign(SA.coeffs(counter)[SA.basis(counter)[w]]),
-                    )
-                        push!(get!(back, w, Tuple{Int,Int}[]), (i, j))
-                    else
-                        delete(i, j)
-                    end
+        for (j, mono) in enumerate(multipliers_gram_monos[i])
+            MA.operate_to!(
+                cache3,
+                *,
+                # Dummy coef to help convert to `SignCount` which is the `eltype` of `cache`
+                _term(SignChange(1, 1), mono),
+                MB.algebra_element(mono),
+            )
+            # The `eltype` of `cache` is `SignCount`
+            # so there is no risk of term cancellation
+            MA.operate_to!(cache4, *, cache3, generators[i])
+            for w in SA.supp(cache4)
+                if ismissing(_sign(SA.coeffs(counter)[SA.basis(counter)[w]]))
+                    push!(get!(back, w, Tuple{Int,Int}[]), (i, j))
+                else
+                    delete(i, j)
                 end
             end
         end
