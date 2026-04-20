040611b334fd149adc0860d611adcbc12742c665
Benoît Legat <benoit.legat@gmail.com>
2026-04-20 11:24:54 +0200

    Simpler mapreduce

--- src/sosdec.jl
+++ src/sosdec.jl
@@ -26,11 +26,7 @@ function MP.polynomial_type(
 end
 
 function GramMatrix(p::SOSDecomposition{T}) where {T}
-    sub_bases = [MB.explicit_basis(a) for a in p.ps]
-    basis = reduce(
-        (b1, b2) -> SA.merge_bases(SA.promote_bases(b1, b2)...),
-        sub_bases,
-    )
+    basis = mapreduce(SA.basis, (b1, b2) -> SA.merge_bases(SA.promote_bases(b1, b2)...), p.ps)
     m = length(p.ps)
     n = length(basis)
     Q = zeros(T, m, n)
