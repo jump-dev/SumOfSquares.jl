89c25aeb4d189371588dea74e59546dc84974f8a
Benoît Legat <benoit.legat@gmail.com>
2026-04-22 17:57:39 +0200

    Add test

--- test/Bridges/Variable/kernel.jl
+++ test/Bridges/Variable/kernel.jl
@@ -68,6 +68,57 @@ function test_runtests()
     return
 end
 
+function test_runtests_weighted()
+    @polyvar x y
+    MOI.Bridges.runtests(
+        SumOfSquares.Bridges.Variable.KernelBridge,
+        model -> begin
+            p, _ = MOI.add_constrained_variables(
+                model,
+                SumOfSquares.WeightedSOSCone{
+                    MOI.PositiveSemidefiniteConeTriangle,
+                }(
+                    MB.SubBasis{MB.Monomial}([
+                        y^4,
+                        x * y^3,
+                        x^2 * y^2,
+                        x^3 * y,
+                        x^4,
+                    ]),
+                    [MB.SubBasis{MB.Monomial}([y^2, x * y, x^2])],
+                    [MB.algebra_element(2.0 * x^0 * y^0)],
+                ),
+            )
+            a = float.(1:length(p))
+            MOI.add_constraint(
+                model,
+                MOI.Utilities.vectorize([a' * p]),
+                MOI.Zeros(1),
+            )
+        end,
+        model -> begin
+            q, _ = MOI.add_constrained_variables(
+                model,
+                MOI.PositiveSemidefiniteConeTriangle(3),
+            )
+            a = float.(1:length(q))
+            MOI.add_constraint(
+                model,
+                MOI.Utilities.vectorize([
+                    2.0 * q[1] +
+                    8.0 * q[2] +
+                    6.0 * (1.0q[3] + 2.0q[4]) +
+                    16.0 * q[5] +
+                    10.0 * q[6],
+                ]),
+                MOI.Zeros(1),
+            )
+        end;
+        cannot_unbridge = true,
+    )
+    return
+end
+
 end  # module
 
 TestVariableKernel.runtests()
