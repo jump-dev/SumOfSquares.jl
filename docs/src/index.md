6ee944acf36228c3c420f9f608c2c36174f89096
Benoît Legat <benoit.legat@gmail.com>
2026-06-16 20:15:44 +0200

    up

--- docs/src/index.md
+++ docs/src/index.md
@@ -6,6 +6,8 @@ enabling the creation of sum of squares variables and constraints in [JuMP](http
 The polynomial can be represented by any library implementing the [MultivariatePolynomial.jl](https://github.com/JuliaAlgebra/MultivariatePolynomials.jl) interface.
 That is, you can currently choose between [DynamicPolynomials](https://github.com/JuliaAlgebra/MultivariatePolynomials.jl) and [TypedPolynomials](https://github.com/JuliaAlgebra/MultivariatePolynomials.jl).
 As a rule of thumb, if you know at compile time (or at the time you are writing your code), the number of variable and that this number is small, use TypedPolynomials, otherwise, use DynamicPolynomials.
+Starting with v0.8, we also have experimental support for any algebra implementing
+[StarAlgebras.jl](https://github.com/JuliaAlgebra/StarAlgebras.jl/). See [CHSH](@ref) for an example of how to define a custom algebra and use it with SumOfSquares.jl.
 
 ## Contents
 ```@contents
--- docs/src/tutorials/Getting started/sampling.jl
+++ docs/src/tutorials/Getting started/sampling.jl
@@ -8,52 +8,43 @@ using Test #src
 using DynamicPolynomials
 using SumOfSquares
 import MultivariateBases as MB
-import SDPLR
+
+using Dualization
 import Hypatia
 import SCS
-import BMSOS
 
-# In this tutorial, we show how to use a different polynomial basis
-# for enforcing the equality between the polynomial and its Sum-of-Squares decomposition.
+# In this tutorial, we show how to use a sampling basis for enforcing
+# the equality between the polynomial and its Sum-of-Squares decomposition.
+# As introduced in [Zero basis](@ref), we can set the zero basis to a sampling
+# basis using the `zero_basis` keyword.
 
 @polyvar x
 p = x^4 - 4x^3 - 2x^2 + 12x + 3
 
-scs = SCS.Optimizer
-sdplr = optimizer_with_attributes(SDPLR.Optimizer, "maxrank" => (m, n) -> 4)
-hypatia = Hypatia.Optimizer
-bmsos = BMSOS.Optimizer
-function test(solver, feas::Bool)
-    model = Model(solver)
-    set_silent(model)
-    if feas
-        γ = -6
-    else
-        @variable(model, γ)
-        @objective(model, Max, γ)
-    end
-    @constraint(model, p - γ in SOSCone(), zero_basis = BoxSampling([-1], [1]))
-    optimize!(model)
-    @test primal_status == MOI.FEASIBLE_POINT
-    if !feasible
-        @test value(γ) ≈ -6 rtol=1e-4
-    end
-end
-test(scs)
-test(sdplr)
-test(hypatia)
-test(bmsos, true)
+model = Model(dual_optimizer(SCS.Optimizer))
+set_silent(model)
+@variable(model, γ)
+@objective(model, Max, γ)
+@constraint(model, p - γ in SOSCone(), zero_basis = BoxSampling([-1], [1]))
+optimize!(model)
+solution_summary(model)
+@test primal_status(model) == MOI.FEASIBLE_POINT #src
+@test value(γ) ≈ -6 rtol=1e-4 #src
 
-function test_rand(solver, d, B)
-    model = Model(solver)
-    set_silent(model)
-    p = MB.algebra_element(rand(2d+1), MB.SubBasis{B}(monomials(x, 0:2d)))
-    @constraint(model, p in SOSCone(), zero_basis = BoxSampling([-1], [1]))
-    optimize!(model)
-    return solve_time(model)
-end
+# We can see that the SOS constraint is converted into a PSD constraint.
 
-test_rand(scs, 100, MultivariateBases.Trigonometric)
-test_rand(sdplr, 100, MultivariateBases.Trigonometric)
-test_rand(hypatia, 100, MultivariateBases.Trigonometric)
-test_rand(bmsos, 100, MultivariateBases.Trigonometric)
+print_active_bridges(model)
+
+# Let's try with Hypatia now:
+
+set_optimizer(model, dual_optimizer(Hypatia.Optimizer))
+optimize!(model)
+solution_summary(model)
+@test primal_status(model) == MOI.FEASIBLE_POINT #src
+@test value(γ) ≈ -6 rtol=1e-4 #src
+
+# We can see that the SOS constraint is passed as a
+# `LowRankOpt.SetDotProducts{LowRankOpt.WITHOUT_SET}`. This maps into
+# Hypatia's native `WSOSInterpNonnegativeCone`.
+
+print_active_bridges(model)
